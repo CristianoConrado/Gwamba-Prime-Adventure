12cf09fa63709ed9fd3a3c0fc2423886b12c96b4
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-13 16:36:43 -0300

    Update Point Setter.cs

--- Characters/Point Setter.cs
+++ Characters/Point Setter.cs
@@ -4,7 +4,7 @@ using NaughtyAttributes;
 using GwambaPrimeAdventure.Connection;
 namespace GwambaPrimeAdventure.Character
 {
-	[DisallowMultipleComponent, Icon("Assets/Huds/Designs/Gwamba Prime Series Logo.png"), RequireComponent(typeof(Transform), typeof(BoxCollider2D))]
+	[DisallowMultipleComponent, Icon("Assets/Huds/Designs/Gwamba Prime Series Logo Show.png"), RequireComponent(typeof(Transform), typeof(BoxCollider2D))]
 	internal sealed class PointSetter : StateController, IConnector
 	{
 		private static PointSetter _instance;
