f9e342ba540655cd9b1e285f8e0636b341644336
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-13 16:37:37 -0300

    Update Gwamba Canvas.cs

--- Characters/Gwamba Canvas.cs
+++ Characters/Gwamba Canvas.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using NaughtyAttributes;
 namespace GwambaPrimeAdventure.Character
 {
-	[DisallowMultipleComponent, Icon("Assets/Huds/Designs/Gwamba Prime Series Logo.png"), RequireComponent(typeof(Transform), typeof(UIDocument))]
+	[DisallowMultipleComponent, Icon("Assets/Huds/Designs/Gwamba Prime Series Logo Show.png"), RequireComponent(typeof(Transform), typeof(UIDocument))]
 	internal sealed class GwambaCanvas : MonoBehaviour
 	{
 		private static GwambaCanvas _instance;
