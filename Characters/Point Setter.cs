2c6dff3f373f11e7f4953b4f84c2e3adabe7df37
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-14 19:33:20 -0300

    Update Point Setter.cs

--- Characters/Point Setter.cs
+++ Characters/Point Setter.cs
@@ -9,14 +9,19 @@ namespace GwambaPrimeAdventure.Character
 	{
 		private static PointSetter _instance;
 		[SerializeField, BoxGroup("Hubby World Interaction"), Tooltip("The name of the hubby world scene."), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F)] private SceneField _hubbyWorldScene;
+		[SerializeField, BoxGroup("Hubby World Interaction"), Tooltip("If this point setter is the begining point to begin in the scene.")] private bool _beginingPoint;
 		[SerializeField, BoxGroup("Hubby World Interaction"), Tooltip("Which point setter is setted when scene is the hubby world.")] private ushort _selfIndex;
 		internal static Vector2 CheckedPoint => _instance ? _instance.transform.position : Vector2.zero;
 		public IEnumerator Load()
 		{
 			SaveController.Load(out SaveFile saveFile);
 			if (gameObject.scene.name == _hubbyWorldScene && !string.IsNullOrEmpty(saveFile.LastLevelEntered))
+			{
 				if (saveFile.LastLevelEntered.Contains($"{_selfIndex}"))
 					_instance = this;
+			}
+			else if (_beginingPoint)
+				_instance = this;
 			yield return null;
 		}
 		private void OnTriggerEnter2D(Collider2D other)
