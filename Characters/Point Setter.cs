b70898d8ecdd98cbceb1a14c639c668ace142781
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-14 13:29:25 -0300

    Update Point Setter.cs

--- Characters/Point Setter.cs
+++ Characters/Point Setter.cs
@@ -5,43 +5,24 @@ using GwambaPrimeAdventure.Connection;
 namespace GwambaPrimeAdventure.Character
 {
 	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(BoxCollider2D))]
-	internal sealed class PointSetter : StateController, IConnector
+	internal sealed class PointSetter : StateController, ILoader
 	{
 		private static PointSetter _instance;
-		private readonly Sender _sender = Sender.Create();
 		[SerializeField, BoxGroup("Hubby World Interaction"), Tooltip("The name of the hubby world scene."), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F)] private SceneField _hubbyWorldScene;
 		[SerializeField, BoxGroup("Hubby World Interaction"), Tooltip("Which point setter is setted when scene is the hubby world.")] private ushort _selfIndex;
-		public MessagePath Path => MessagePath.Character;
-		private new void Awake()
+		internal static Vector2 CheckedPoint => _instance ? _instance.transform.position : Vector2.zero;
+		public IEnumerator Load()
 		{
-			base.Awake();
-			_sender.SetFormat(MessageFormat.Event);
-			_sender.SetAdditionalData((Vector2)transform.position);
-			_sender.SetToggle(false);
-			Sender.Include(this);
-		}
-		private new void OnDestroy()
-		{
-			base.OnDestroy();
-			Sender.Exclude(this);
-		}
-		private IEnumerator Start()
-		{
-			yield return new WaitWhile(() => SceneInitiator.IsInTrancision());
 			SaveController.Load(out SaveFile saveFile);
 			if (gameObject.scene.name == _hubbyWorldScene && !string.IsNullOrEmpty(saveFile.LastLevelEntered))
 				if (saveFile.LastLevelEntered.Contains($"{_selfIndex}"))
-					_sender.Send(MessagePath.Character);
+					_instance = this;
+			yield return null;
 		}
 		private void OnTriggerEnter2D(Collider2D other)
 		{
 			if (GwambaStateMarker.EqualObject(other.gameObject) && this != _instance)
 				_instance = this;
-		}
-		public void Receive(MessageData message)
-		{
-			if (MessageFormat.Event == message.Format && message.ToggleValue.HasValue && message.ToggleValue.Value && this == _instance)
-				_sender.Send(MessagePath.Character);
 		}
 	};
 };
