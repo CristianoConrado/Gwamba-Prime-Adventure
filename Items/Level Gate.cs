4706d69063b6a7a217b55b595e9c6636c5caf218
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-16 14:11:09 -0300

    Update Level Gate.cs

--- Items/Level Gate.cs
+++ Items/Level Gate.cs
@@ -10,21 +10,23 @@ namespace GwambaPrimeAdventure.Item
 	[RequireComponent(typeof(Transitioner), typeof(IInteractable))]
 	internal sealed class LevelGate : MonoBehaviour, ILoader
 	{
-		private LevelGateHud _levelGate;
+		private LevelGateHud
+			_levelGateWorld,
+			_levelGateScreen;
 		private CinemachineCamera _gateCamera;
 		private readonly Sender _sender = Sender.Create();
-		private Vector2 _transitionSize = Vector2.zero;
-		private Vector2 _worldSpaceSize = Vector2.zero;
-		private Vector2 _activeSize = Vector2.one;
-		private Vector2 _defaultUISize = new(WorldBuild.UI_SCALE_WIDTH, WorldBuild.UI_SCALE_HEIGHt);
-		private bool _isOnInteraction = false;
-		private bool _isOnTransicion = false;
-		private short _defaultPriority;
+		private Vector2
+			_transitionSize = Vector2.zero,
+			_worldSpaceSize = Vector2.zero,
+			_activeSize = Vector2.one;
+		private short _defaultPriority = 0;
+		private bool
+			_isOnInteraction = false,
+			_isOnTransicion = false;
 		[Header("Scene Status")]
 		[SerializeField, Tooltip("The brain responsable for controlling the camera.")] private CinemachineBrain _brain;
-		[SerializeField, Tooltip("The object that handles the hud of the level gate.")] private LevelGateHud _levelGateObject;
-		[SerializeField, Tooltip("The panel settings of the world space.")] private PanelSettings _worldSpacePanelSettings;
-		[SerializeField, Tooltip("The panel settings of the screen space's overlay.")] private PanelSettings _screenOverlayPanelSettings;
+		[SerializeField, Tooltip("The object that handles the hud of the level gate.")] private LevelGateHud _levelGateWorldObject;
+		[SerializeField, Tooltip("The object that handles the hud of the level gate.")] private LevelGateHud _levelGateScreenObject;
 		[SerializeField, Tooltip("The scene of the level.")] private SceneField _levelScene;
 		[SerializeField, Tooltip("The scene of the boss.")] private SceneField _bossScene;
 		[SerializeField, Tooltip("The offset that the hud will be.")] private Vector2 _offsetPosition;
@@ -34,25 +36,36 @@ namespace GwambaPrimeAdventure.Item
 			_gateCamera = GetComponentInChildren<CinemachineCamera>();
 			_sender.SetFormat(MessageFormat.Event);
 			_sender.SetAdditionalData(gameObject);
-			_levelGate = Instantiate(_levelGateObject, transform);	
 		}
 		private void OnDestroy()
 		{
-			_levelGate.Level.clicked -= EnterLevel;
-			_levelGate.Boss.clicked -= EnterBoss;
-			_levelGate.Scenes.clicked -= ShowScenes;
+			if (_levelGateWorld)
+				Destroy(_levelGateWorld.gameObject);
+			if (_levelGateScreen)
+			{
+				_levelGateScreen.Level.clicked -= EnterLevel;
+				_levelGateScreen.Boss.clicked -= EnterBoss;
+				_levelGateScreen.Scenes.clicked -= ShowScenes;
+				Destroy(_levelGateScreen.gameObject);
+			}
 		}
 		public IEnumerator Load()
 		{
-			_levelGate.transform.localPosition = _offsetPosition;
-			_transitionSize = _worldSpaceSize = _levelGate.Document.worldSpaceSize;
+			(_levelGateWorld, _levelGateScreen) = (Instantiate(_levelGateWorldObject, transform), Instantiate(_levelGateScreenObject, transform));
+			_levelGateScreen.RootElement.style.display = DisplayStyle.None;
+			_levelGateScreen.Level.SetEnabled(true);
+			_levelGateScreen.Boss.SetEnabled(true);
+			_levelGateScreen.Scenes.SetEnabled(true);
+			_levelGateWorld.transform.localPosition = _offsetPosition;
+			_levelGateScreen.transform.localPosition = _offsetPosition;
+			_transitionSize = _worldSpaceSize = _levelGateWorld.Document.worldSpaceSize;
 			_activeSize = WorldBuild.OrthographicToScreenSize(_gateCamera.Lens.OrthographicSize);
 			SaveController.Load(out SaveFile saveFile);
-			_levelGate.Level.clicked += EnterLevel;
+			_levelGateScreen.Level.clicked += EnterLevel;
 			if (saveFile.LevelsCompleted[ushort.Parse($"{_levelScene.SceneName[^1]}") - 1])
-				_levelGate.Boss.clicked += EnterBoss;
+				_levelGateScreen.Boss.clicked += EnterBoss;
 			if (saveFile.DeafetedBosses[ushort.Parse($"{_levelScene.SceneName[^1]}") - 1])
-				_levelGate.Scenes.clicked += ShowScenes;
+				_levelGateScreen.Scenes.clicked += ShowScenes;
 			_defaultPriority = (short)_gateCamera.Priority.Value;
 			yield return null;
 		}
@@ -68,22 +81,19 @@ namespace GwambaPrimeAdventure.Item
 			_isOnTransicion = true;
 			float time;
 			float elapsedTime = 0F;
-			while (_isOnInteraction && _levelGate.Document.worldSpaceSize != _activeSize)
+			while (_isOnInteraction && _levelGateWorld.Document.worldSpaceSize != _activeSize)
 			{
 				time = elapsedTime / _brain.DefaultBlend.Time;
-				_levelGate.Document.worldSpaceSize = Vector2.Lerp(_transitionSize, _activeSize, time);
+				_levelGateWorld.Document.worldSpaceSize = Vector2.Lerp(_transitionSize, _activeSize, time);
 				elapsedTime = elapsedTime >= _brain.DefaultBlend.Time ? _brain.DefaultBlend.Time : elapsedTime + Time.deltaTime;
 				yield return null;
 			}
-			_transitionSize = _levelGate.Document.worldSpaceSize;
+			_transitionSize = _levelGateWorld.Document.worldSpaceSize;
 			_isOnTransicion = false;
 			if (!_isOnInteraction)
 				yield break;
-			_levelGate.Level.SetEnabled(true);
-			_levelGate.Boss.SetEnabled(true);
-			_levelGate.Scenes.SetEnabled(true);
-			_levelGate.Document.worldSpaceSize = _defaultUISize;
-			_levelGate.Document.panelSettings = _screenOverlayPanelSettings;
+			_levelGateWorld.RootElement.style.display = DisplayStyle.None;
+			_levelGateScreen.RootElement.style.display = DisplayStyle.Flex;
 		}
 		private IEnumerator OffHud()
 		{
@@ -91,22 +101,20 @@ namespace GwambaPrimeAdventure.Item
 			while (_isOnTransicion)
 				yield return null;
 			_gateCamera.Priority.Value = _defaultPriority;
-			_levelGate.Level.SetEnabled(false);
-			_levelGate.Boss.SetEnabled(false);
-			_levelGate.Scenes.SetEnabled(false);
-			_levelGate.Document.panelSettings = _worldSpacePanelSettings;
-			_levelGate.Document.worldSpaceSize = _activeSize;
+			_levelGateScreen.RootElement.style.display = DisplayStyle.None;
+			_levelGateWorld.RootElement.style.display = DisplayStyle.Flex;
+			_levelGateWorld.Document.worldSpaceSize = _activeSize;
 			_isOnTransicion = true;
 			float time;
 			float elapsedTime = 0F;
-			while (!_isOnInteraction && _levelGate.Document.worldSpaceSize != _worldSpaceSize)
+			while (!_isOnInteraction && _levelGateWorld.Document.worldSpaceSize != _worldSpaceSize)
 			{
 				time = elapsedTime / _brain.DefaultBlend.Time;
-				_levelGate.Document.worldSpaceSize = Vector2.Lerp(_transitionSize, _worldSpaceSize, time);
+				_levelGateWorld.Document.worldSpaceSize = Vector2.Lerp(_transitionSize, _worldSpaceSize, time);
 				elapsedTime = elapsedTime >= _brain.DefaultBlend.Time ? _brain.DefaultBlend.Time : elapsedTime + Time.deltaTime;
 				yield return null;
 			}
-			_transitionSize = _levelGate.Document.worldSpaceSize;
+			_transitionSize = _levelGateWorld.Document.worldSpaceSize;
 			_isOnTransicion = false;
 		}
 		private void OnTriggerEnter2D(Collider2D other)
