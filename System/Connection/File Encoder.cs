a470fc32154428b58bc852350e1d141b534df0ab
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-18 11:29:59 -0300

    Update File Encoder.cs

--- System/Connection/File Encoder.cs
+++ System/Connection/File Encoder.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
 using System.IO;
 using System.Text;
+using System;
 namespace GwambaPrimeAdventure.Connection
 {
 	internal static class FileEncoder
 	{
-		private const string ScriptPassword = "";
+		private const string ScriptPassword = "BoLo%De%CeNoUrA%cOm%CoBeRtUrA%dE%cHoCoLaTe%AmArGo%!";
 		private static string ScriptData(string data)
 		{
-			string scriptedData = "";
+			Span<char> buffer = stackalloc char[data.Length];
 			for (ushort i = 0; data.Length > i; i++)
-				scriptedData += (char)(data[i] ^ ScriptPassword[i % ScriptPassword.Length]);
-			return scriptedData;
+				buffer[i] = (char)(data[i] ^ ScriptPassword[i % ScriptPassword.Length]);
+			return new string(buffer);
 		}
 		internal static Data ReadData<Data>(string path) where Data : struct => JsonUtility.FromJson<Data>(ScriptData(File.ReadAllText(path, Encoding.UTF8)));
 		internal static void WriteData<Data>(Data structData, string path) where Data : struct
