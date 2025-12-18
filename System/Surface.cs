a069fd5f7f9565a4ae4383772c0b3c3b97720c43
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-18 15:57:56 -0300

    Update Surface.cs

--- System/Surface.cs
+++ System/Surface.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Collections;
+using System.Collections.Generic;
 namespace GwambaPrimeAdventure
 {
 	[DisallowMultipleComponent, Icon(WorldBuild.PROJECT_ICON), RequireComponent(typeof(Transform), typeof(Tilemap), typeof(TilemapRenderer)), RequireComponent(typeof(TilemapCollider2D))]
 	public sealed class Surface : MonoBehaviour, ILoader
 	{
+		private static readonly List<Surface> _surfaces = new();
 		private Tilemap _tilemap;
+		private Tile _returnedTile;
+		private void OnEnable() => _surfaces.Add(this);
+		private void OnDisable() => _surfaces.Remove(this);
 		public IEnumerator Load()
 		{
 			_tilemap = GetComponent<Tilemap>();
 			yield return null;
 		}
-		public Tile CheckForTile(Vector2 originPosition) => _tilemap.GetTile<Tile>(_tilemap.WorldToCell(originPosition));
+		public static Tile CheckForTile(Vector2 originPosition)
+		{
+			foreach (Surface surface in _surfaces)
+				if ((surface._returnedTile = surface._tilemap.GetTile<Tile>(surface._tilemap.WorldToCell(originPosition))) && surface._tilemap.ContainsTile(surface._returnedTile))
+					return surface._returnedTile;
+			return null;
+		}
 	};
 };
