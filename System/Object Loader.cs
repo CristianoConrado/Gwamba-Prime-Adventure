c3b065394f391f91a6612fc4c8cb461245756245
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 19:11:28 -0300

    Update Object Loader.cs

--- System/Object Loader.cs
+++ System/Object Loader.cs
@@ -4,28 +4,28 @@ using System.Collections;
 using System.Collections.Generic;
 namespace GwambaPrimeAdventure
 {
-	[Icon(WorldBuild.PROJECT_ICON)]
+	[Icon( WorldBuild.PROJECT_ICON )]
 	internal sealed class ObjectLoader : MonoBehaviour
 	{
 		private static readonly List<ILoader> _loader = new();
-		public IEnumerator Load(ProgressBar progressBar)
+		public IEnumerator Load( ProgressBar progressBar )
 		{
 			_loader.Clear();
-			GetComponentsInChildren<ILoader>(_loader);
+			GetComponentsInChildren<ILoader>( _loader );
 			float progress = 0F;
-			for (ushort i = 0; _loader.Count > i; i++)
+			for ( ushort i = 0; _loader.Count > i; i++ )
 			{
-				yield return StartCoroutine(_loader[i].Load());
+				yield return StartCoroutine( _loader[ i ].Load() );
 				progressBar.value -= progress;
-				progressBar.value += progress = (i + 1F) / _loader.Count;
+				progressBar.value += progress = ( i + 1F ) / _loader.Count;
 			}
-			if (0 >= _loader.Count)
+			if ( 0 >= _loader.Count )
 				progressBar.value += ++SceneInitiator.ProgressIndex / SceneInitiator.ProgressIndex;
 			else
 				progressBar.value += ++SceneInitiator.ProgressIndex - progressBar.value;
 			_loader.Clear();
 			transform.DetachChildren();
-			Destroy(gameObject);
+			Destroy( gameObject );
 		}
 	};
 };
