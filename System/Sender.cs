e3307a3ec393edafade64de384195eb7d3e6e0d1
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 19:12:58 -0300

    Update Sender.cs

--- System/Sender.cs
+++ System/Sender.cs
@@ -18,35 +18,35 @@ namespace GwambaPrimeAdventure
 		}
 		private static readonly ConcurrentDictionary<MessagePath, HashSet<IConnector>> _connectors = new();
 		private MessageData _messageData;
-		public static ValueTask Include(IConnector connector)
+		public static ValueTask Include( IConnector connector )
 		{
-			HashSet<IConnector> gettedConnectors = _connectors.GetOrAdd(connector.Path, _ => new HashSet<IConnector>());
-			gettedConnectors.Add(connector);
-			return new ValueTask(Task.CompletedTask);
+			HashSet<IConnector> gettedConnectors = _connectors.GetOrAdd( connector.Path, _ => new HashSet<IConnector>() );
+			gettedConnectors.Add( connector );
+			return new ValueTask( Task.CompletedTask );
 		}
-		public static ValueTask Exclude(IConnector connector)
+		public static ValueTask Exclude( IConnector connector )
 		{
-			if (_connectors.TryGetValue(connector.Path, out HashSet<IConnector> gettedConnectors))
+			if ( _connectors.TryGetValue( connector.Path, out HashSet<IConnector> gettedConnectors ) )
 			{
-				gettedConnectors.Remove(connector);
-				if (0 >= gettedConnectors.Count)
-					_connectors.TryRemove(connector.Path, out _);
+				gettedConnectors.Remove( connector );
+				if ( 0 >= gettedConnectors.Count )
+					_connectors.TryRemove( connector.Path, out _ );
 			}
-			return new ValueTask(Task.CompletedTask);
+			return new ValueTask( Task.CompletedTask );
 		}
 		public static Sender Create() => new();
-		public void SetFormat(MessageFormat format) => _messageData.Format = format;
-		public void SetAdditionalData(object additionalData) => _messageData.AdditionalData = additionalData;
-		public void SetToggle(bool toggle) => _messageData.ToggleValue = toggle;
-		public void SetNumber(ushort number) => _messageData.NumberValue = number;
-		public void Send(MessagePath path)
+		public void SetFormat( MessageFormat format ) => _messageData.Format = format;
+		public void SetAdditionalData( object additionalData ) => _messageData.AdditionalData = additionalData;
+		public void SetToggle( bool toggle ) => _messageData.ToggleValue = toggle;
+		public void SetNumber( ushort number ) => _messageData.NumberValue = number;
+		public void Send( MessagePath path )
 		{
-			if (_connectors.TryGetValue(path, out HashSet<IConnector> connectors))
+			if ( _connectors.TryGetValue( path, out HashSet<IConnector> connectors ) )
 			{
-				IConnector[] snapshot = new IConnector[connectors.Count];
-				connectors.CopyTo(snapshot);
-				foreach (IConnector connector in snapshot.AsSpan())
-					connector?.Receive(_messageData);
+				IConnector[] snapshot = new IConnector[ connectors.Count ];
+				connectors.CopyTo( snapshot );
+				foreach ( IConnector connector in snapshot.AsSpan() )
+					connector?.Receive( _messageData );
 			}
 		}
 	};
