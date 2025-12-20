8434748c571e05c0d02c0c078ac0a733846dd3cc
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:41:44 -0300

    Update Teleport Point.cs

--- Enemies/Utilities/Teleport Point.cs
+++ Enemies/Utilities/Teleport Point.cs
@@ -2,30 +2,26 @@ using UnityEngine;
 using GwambaPrimeAdventure.Character;
 namespace GwambaPrimeAdventure.Enemy.Utility
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
+	[DisallowMultipleComponent, RequireComponent( typeof( Transform ), typeof( Collider2D ) )]
 	public sealed class TeleportPoint : StateController
 	{
 		private ITeleporter _teleporter;
 		private ushort _teleportIndex;
-		[Header("Interactions")]
-		[SerializeField, Tooltip("If this point will destroy itself after use.")] private bool _destroyAfter;
-		[SerializeField, Tooltip("If this point will trigger with other object.")] private bool _hasTarget;
-		public void GetTouch(ITeleporter teleporter, ushort teleportIndex)
+		[Header( "Interactions" )]
+		[SerializeField, Tooltip( "If this point will destroy itself after use." )] private bool _destroyAfter;
+		[SerializeField, Tooltip( "If this point will trigger with other object." )] private bool _hasTarget;
+		public void GetTouch( ITeleporter teleporter, ushort teleportIndex ) => (_teleporter, _teleportIndex) = (teleporter, teleportIndex);
+		private void OnTriggerEnter2D( Collider2D other )
 		{
-			_teleporter = teleporter;
-			_teleportIndex = teleportIndex;
-		}
-		private void OnTriggerEnter2D(Collider2D other)
-		{
-			if (_hasTarget)
+			if ( _hasTarget )
 			{
-				if (GwambaStateMarker.EqualObject(other.gameObject))
-					_teleporter.OnTeleport(_teleportIndex);
+				if ( CharacterExporter.EqualGwamba( other.gameObject ) )
+					_teleporter.OnTeleport( _teleportIndex );
 			}
-			else if (other.TryGetComponent<ITeleporter>(out _))
-				_teleporter.OnTeleport(_teleportIndex);
-			if (_destroyAfter)
-				Destroy(gameObject);
+			else if ( other.TryGetComponent<ITeleporter>( out _ ) )
+				_teleporter.OnTeleport( _teleportIndex );
+			if ( _destroyAfter )
+				Destroy( gameObject );
 		}
 	};
 };
