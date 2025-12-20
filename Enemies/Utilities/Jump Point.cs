4fcc666cc8085c7e12f6d6d826457f8eb87cfdd9
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:39:22 -0300

    Update Jump Point.cs

--- Enemies/Utilities/Jump Point.cs
+++ Enemies/Utilities/Jump Point.cs
@@ -2,30 +2,26 @@ using UnityEngine;
 using GwambaPrimeAdventure.Character;
 namespace GwambaPrimeAdventure.Enemy.Utility
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
+	[DisallowMultipleComponent, RequireComponent( typeof( Transform ), typeof( Collider2D ) )]
 	public sealed class JumpPoint : StateController
 	{
 		private IJumper _jumper;
 		private ushort _touchIndex;
-		[Header("Interactions")]
-		[SerializeField, Tooltip("If this point will destroy itself after use.")] private bool _destroyAfter;
-		[SerializeField, Tooltip("If this point will trigger with other object.")] private bool _hasTarget;
-		public void GetTouch(IJumper jumperEnemy, ushort touchIndex)
+		[Header( "Interactions" )]
+		[SerializeField, Tooltip( "If this point will destroy itself after use." )] private bool _destroyAfter;
+		[SerializeField, Tooltip( "If this point will trigger with other object." )] private bool _hasTarget;
+		public void GetTouch( IJumper jumperEnemy, ushort touchIndex ) => (_jumper, _touchIndex) = (jumperEnemy, touchIndex);
+		private void OnTriggerEnter2D( Collider2D other )
 		{
-			_jumper = jumperEnemy;
-			_touchIndex = touchIndex;
-		}
-		private void OnTriggerEnter2D(Collider2D other)
-		{
-			if (_hasTarget)
+			if ( _hasTarget )
 			{
-				if (GwambaStateMarker.EqualObject(other.gameObject))
-					_jumper.OnJump(_touchIndex);
+				if ( CharacterExporter.EqualGwamba( other.gameObject ) )
+					_jumper.OnJump( _touchIndex );
 			}
-			else if (other.TryGetComponent<IJumper>(out _))
-				_jumper.OnJump(_touchIndex);
-			if (_destroyAfter)
-				Destroy(gameObject);
+			else if ( other.TryGetComponent<IJumper>( out _ ) )
+				_jumper.OnJump( _touchIndex );
+			if ( _destroyAfter )
+				Destroy( gameObject );
 		}
 	};
 };
