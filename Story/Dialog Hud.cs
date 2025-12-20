aad73d7f9f3d8677d00829b6ede08bdf096f69b6
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 19:02:24 -0300

    Update Dialog Hud.cs

--- Story/Dialog Hud.cs
+++ Story/Dialog Hud.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 namespace GwambaPrimeAdventure.Story
 {
-	[DisallowMultipleComponent, Icon(WorldBuild.PROJECT_ICON), RequireComponent(typeof(Transform), typeof(UIDocument))]
+	[DisallowMultipleComponent, Icon( WorldBuild.PROJECT_ICON ), RequireComponent( typeof( Transform ), typeof( UIDocument ) )]
 	internal sealed class DialogHud : MonoBehaviour
 	{
 		static private DialogHud _instance;
@@ -13,18 +13,14 @@ namespace GwambaPrimeAdventure.Story
 		internal Button AdvanceSpeach { get; private set; }
 		private void Awake()
 		{
-			if (_instance)
+			if ( _instance )
 			{
-				Destroy(gameObject, WorldBuild.MINIMUM_TIME_SPACE_LIMIT);
+				Destroy( gameObject, WorldBuild.MINIMUM_TIME_SPACE_LIMIT );
 				return;
 			}
-			_instance = this;
-			RootElement = GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>(nameof(RootElement));
-			CharacterIcon = RootElement.Q<VisualElement>(nameof(CharacterIcon));
-			CharacterName = RootElement.Q<Label>(nameof(CharacterName));
-			CharacterSpeach = RootElement.Q<Label>(nameof(CharacterSpeach));
-			AdvanceSpeach = RootElement.Q<Button>(nameof(AdvanceSpeach));
+			(_instance, RootElement) = (this, GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>( nameof( RootElement ) ));
+			(CharacterIcon, CharacterName) = (RootElement.Q<VisualElement>( nameof( CharacterIcon ) ), RootElement.Q<Label>( nameof( CharacterName ) ));
+			(CharacterSpeach, AdvanceSpeach) = (RootElement.Q<Label>( nameof( CharacterSpeach ) ), RootElement.Q<Button>( nameof( AdvanceSpeach ) ));
 		}
 	};
 };
-
