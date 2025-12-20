3ba0dff7cc066c726dc17ae64fc643ca7aa254f9
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:35:34 -0300

    Update Defender Statistics.cs

--- Enemies/Utilities/Defender Statistics.cs
+++ Enemies/Utilities/Defender Statistics.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 using NaughtyAttributes;
 namespace GwambaPrimeAdventure.Enemy.Utility
 {
-	[CreateAssetMenu(fileName = "Defender Enemy", menuName = "Enemy Statistics/Defender", order = 8)]
+	[CreateAssetMenu( fileName = "Defender Enemy", menuName = "Enemy Statistics/Defender", order = 8 )]
 	public sealed class DefenderStatistics : ScriptableObject
 	{
-		[field: SerializeField, Tooltip("The amount of damage that this object have to receive real damage."), Header("Defender Enemy", order = 0), Space(WorldBuild.FIELD_SPACE_LENGTH * 2F, order = 1)]
+		[field: SerializeField, Tooltip( "The amount of damage that this object have to receive real damage." ), Header( "Defender Enemy", order = 0 ), Space( WorldBuild.FIELD_SPACE_LENGTH * 2F, order = 1 )]
 		public short BiggerDamage { get; private set; }
-		[field: SerializeField, Tooltip("If this enemy will stop moving when become invencible.\nRequires: Moving Enemy.")] public bool InvencibleStop { get; private set; }
-		[field: SerializeField, Tooltip("If this enemy will become invencible when hurted.")] public bool InvencibleHurted { get; private set; }
-		[field: SerializeField, Tooltip("If this enemy will react to any damage taken to a event.")] public bool ReactToDamage { get; private set; }
-		[field: SerializeField, Tooltip("If this enemy will use time to become invencible/destructible.")] public bool UseAlternatedTime { get; private set; }
-		[field: SerializeField, Tooltip("The amount of time the enemy have to become destructible.")] public float TimeToDestructible { get; private set; }
-		[field: SerializeField, ShowIf(nameof(UseAlternatedTime)), Tooltip("The amount of time the enemy have to become invencible.")] public float TimeToInvencible { get; private set; }
+		[field: SerializeField, Tooltip( "If this enemy will stop moving when become invencible.\nRequires: Moving Enemy." )] public bool InvencibleStop { get; private set; }
+		[field: SerializeField, Tooltip( "If this enemy will become invencible when hurted." )] public bool InvencibleHurted { get; private set; }
+		[field: SerializeField, Tooltip( "If this enemy will react to any damage taken to a event." )] public bool ReactToDamage { get; private set; }
+		[field: SerializeField, Tooltip( "If this enemy will use time to become invencible/destructible." )] public bool UseAlternatedTime { get; private set; }
+		[field: SerializeField, Tooltip( "The amount of time the enemy have to become destructible." )] public float TimeToDestructible { get; private set; }
+		[field: SerializeField, ShowIf( nameof( UseAlternatedTime ) ), Tooltip( "The amount of time the enemy have to become invencible." )] public float TimeToInvencible { get; private set; }
 	};
 };
