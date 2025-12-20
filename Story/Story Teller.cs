ca595b34e636081209b6919d3cbb551d7c8f82cd
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 19:03:49 -0300

    Update Story Teller.cs

--- Story/Story Teller.cs
+++ Story/Story Teller.cs
@@ -3,53 +3,52 @@ using UnityEngine.UIElements;
 using System.Collections;
 namespace GwambaPrimeAdventure.Story
 {
-	[DisallowMultipleComponent, Icon(WorldBuild.PROJECT_ICON), RequireComponent(typeof(Transform))]
+	[DisallowMultipleComponent, Icon( WorldBuild.PROJECT_ICON ), RequireComponent( typeof( Transform ) )]
 	internal sealed class StoryTeller : MonoBehaviour
 	{
 		private StorySceneHud _storySceneHud;
 		private ushort _imageIndex = 0;
-		[Header("Scene Objects")]
-		[SerializeField, Tooltip("The object that handles the hud of the story scene.")] private StorySceneHud _storySceneHudObject;
-		[SerializeField, Tooltip("The object that carry the scene settings.")] private StorySceneObject _storySceneObject;
-		private IEnumerator FadeImage(bool appear)
+		[Header( "Scene Objects" )]
+		[SerializeField, Tooltip( "The object that handles the hud of the story scene." )] private StorySceneHud _storySceneHudObject;
+		[SerializeField, Tooltip( "The object that carry the scene settings." )] private StorySceneObject _storySceneObject;
+		private IEnumerator FadeImage( bool appear )
 		{
-			if (appear)
-				for (float i = 0F; 1F > _storySceneHud.SceneImage.style.opacity.value; i += 1E-1F)
+			if ( appear )
+				for ( float i = 0F; 1F > _storySceneHud.SceneImage.style.opacity.value; i += 1E-1F )
 					yield return _storySceneHud.SceneImage.style.opacity = i;
 			else
-				for (float i = 1F; 0F < _storySceneHud.SceneImage.style.opacity.value; i -= 1E-1F)
+				for ( float i = 1F; 0F < _storySceneHud.SceneImage.style.opacity.value; i -= 1E-1F )
 					yield return _storySceneHud.SceneImage.style.opacity = i;
 		}
 		internal void ShowScene()
 		{
-			_storySceneHud = Instantiate(_storySceneHudObject, transform);
-			_storySceneHud.SceneImage.style.backgroundImage = Background.FromTexture2D(_storySceneObject.SceneComponents[_imageIndex = 0].Image);
-			StartCoroutine(FadeImage(true));
+			_storySceneHud = Instantiate( _storySceneHudObject, transform );
+			_storySceneHud.SceneImage.style.backgroundImage = Background.FromTexture2D( _storySceneObject.SceneComponents[ _imageIndex = 0 ].Image );
+			StartCoroutine( FadeImage( true ) );
 		}
 		internal IEnumerator NextSlide()
 		{
-			if (_storySceneObject.SceneComponents[_imageIndex].Equals(_storySceneObject.SceneComponents[^1]))
+			if ( _storySceneObject.SceneComponents[ _imageIndex ].Equals( _storySceneObject.SceneComponents[ ^1 ] ) )
 				yield break;
-			yield return StartCoroutine(FadeImage(false));
-			_imageIndex = (ushort)(_storySceneObject.SceneComponents.Length - 1 > _imageIndex ? _imageIndex + 1 : 0);
-			_storySceneHud.SceneImage.style.backgroundImage = Background.FromTexture2D(_storySceneObject.SceneComponents[_imageIndex].Image);
-			yield return StartCoroutine(FadeImage(true));
-			if (_storySceneObject.SceneComponents[_imageIndex].OffDialog)
+			yield return StartCoroutine( FadeImage( false ) );
+			_imageIndex = (ushort) ( _storySceneObject.SceneComponents.Length - 1 > _imageIndex ? _imageIndex + 1 : 0 );
+			_storySceneHud.SceneImage.style.backgroundImage = Background.FromTexture2D( _storySceneObject.SceneComponents[ _imageIndex ].Image );
+			yield return StartCoroutine( FadeImage( true ) );
+			if ( _storySceneObject.SceneComponents[ _imageIndex ].OffDialog )
 			{
-				yield return new WaitForSeconds(_storySceneObject.SceneComponents[_imageIndex].TimeToDesapear);
-				if (_storySceneObject.SceneComponents[_imageIndex].JumpToNext)
-					yield return StartCoroutine(NextSlide());
+				yield return new WaitForSeconds( _storySceneObject.SceneComponents[ _imageIndex ].TimeToDesapear );
+				if ( _storySceneObject.SceneComponents[ _imageIndex ].JumpToNext )
+					yield return StartCoroutine( NextSlide() );
 			}
 		}
 		internal void CloseScene()
 		{
-			StartCoroutine(DestroyScene());
+			StartCoroutine( DestroyScene() );
 			IEnumerator DestroyScene()
 			{
-				yield return StartCoroutine(FadeImage(false));
-				Destroy(_storySceneHud.gameObject);
+				yield return StartCoroutine( FadeImage( false ) );
+				Destroy( _storySceneHud.gameObject );
 			}
 		}
 	};
 };
-
