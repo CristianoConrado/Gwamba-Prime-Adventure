9b7c22eefbf0c9c0aeaf89581f9814bc962895b9
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 19:12:28 -0300

    Update Scene Field.cs

--- System/Scene Field.cs
+++ System/Scene Field.cs
@@ -9,27 +9,27 @@ namespace GwambaPrimeAdventure
 	public class SceneField
 	{
 #if UNITY_EDITOR
-		[SerializeField, Tooltip("The scene to be handled.")] private SceneAsset _sceneAsset;
+		[SerializeField, Tooltip( "The scene to be handled." )] private SceneAsset _sceneAsset;
 #endif
-		[SerializeField, Tooltip("The name of the scene.")] private string _sceneName;
+		[SerializeField, Tooltip( "The name of the scene." )] private string _sceneName;
 		public string SceneName => _sceneName;
-		public static implicit operator string(SceneField sceneObject) => sceneObject.SceneName;
+		public static implicit operator string( SceneField sceneObject ) => sceneObject.SceneName;
 	};
 #if UNITY_EDITOR
-	[CustomPropertyDrawer(typeof(SceneField))]
+	[CustomPropertyDrawer( typeof( SceneField ) )]
 	public class SceneFieldProperty : PropertyDrawer
 	{
-		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
 		{
-			EditorGUI.BeginProperty(position, GUIContent.none, property);
-			SerializedProperty sceneAsset = property.FindPropertyRelative("_sceneAsset");
-			SerializedProperty sceneName = property.FindPropertyRelative("_sceneName");
-			position =  EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-			if (sceneAsset != null)
+			EditorGUI.BeginProperty( position, GUIContent.none, property );
+			SerializedProperty sceneAsset = property.FindPropertyRelative( "_sceneAsset" );
+			SerializedProperty sceneName = property.FindPropertyRelative( "_sceneName" );
+			position = EditorGUI.PrefixLabel( position, GUIUtility.GetControlID( FocusType.Passive ), label );
+			if ( sceneAsset != null )
 			{
-				sceneAsset.objectReferenceValue = EditorGUI.ObjectField(position, sceneAsset.objectReferenceValue, typeof(SceneAsset), true);
-				if (sceneAsset.objectReferenceValue != null)
-					sceneName.stringValue = (sceneAsset.objectReferenceValue as SceneAsset).name;
+				sceneAsset.objectReferenceValue = EditorGUI.ObjectField( position, sceneAsset.objectReferenceValue, typeof( SceneAsset ), true );
+				if ( sceneAsset.objectReferenceValue != null )
+					sceneName.stringValue = ( sceneAsset.objectReferenceValue as SceneAsset ).name;
 			}
 			EditorGUI.EndProperty();
 		}
