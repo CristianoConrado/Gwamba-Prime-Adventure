1ebf0aecd96647c946ec46c6bf1ce03f7d6e7268
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:10:34 -0300

    Update Hidden Place.cs

--- Items/Event Items/Hidden Place.cs
+++ Items/Event Items/Hidden Place.cs
@@ -7,8 +7,8 @@ using GwambaPrimeAdventure.Character;
 using GwambaPrimeAdventure.Connection;
 namespace GwambaPrimeAdventure.Item.EventItem
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Tilemap), typeof(TilemapRenderer))]
-	[RequireComponent(typeof(TilemapCollider2D), typeof(CompositeCollider2D), typeof(Light2DBase)), RequireComponent(typeof(Receptor))]
+	[DisallowMultipleComponent, RequireComponent( typeof( Transform ), typeof( Tilemap ), typeof( TilemapRenderer ) )]
+	[RequireComponent( typeof( TilemapCollider2D ), typeof( CompositeCollider2D ), typeof( Light2DBase ) ), RequireComponent( typeof( Receptor ) )]
 	internal sealed class HiddenPlace : StateController, IReceptorSignal
 	{
 		private Tilemap _tilemap;
@@ -21,130 +21,130 @@ namespace GwambaPrimeAdventure.Item.EventItem
 		private bool
 			_activation = false,
 			_follow = false;
-		[Header("Hidden Place")]
-		[SerializeField, Tooltip("Other hidden place to activate.")] private HiddenPlace _otherPlace;
-		[SerializeField, Tooltip("The occlusion object to reveal/hide.")] private OcclusionObject _occlusionObject;
-		[SerializeField, Tooltip("If this object will receive a signal.")] private bool _isReceptor;
-		[SerializeField, ShowIf(nameof(_isReceptor)), Tooltip("The amount o time to appear/fade again after the activation.")] private float _timeToFadeAppearAgain;
-		[SerializeField, ShowIf(nameof(_isReceptor)), Tooltip("If the activation of the receive signal will fade the place.")] private bool _fadeActivation;
-		[SerializeField, ShowIf(nameof(_isReceptor)), Tooltip("If this place won't use his own collider.")] private bool _useOtherCollider;
-		[SerializeField, Tooltip("If the other hidden place will appear first.")] private bool _appearFirst;
-		[SerializeField, Tooltip("If the other hidden place will fade first.")] private bool _fadeFirst;
-		[SerializeField, Tooltip("If this object will appear/fade instantly.")] private bool _instantly;
-		[SerializeField, Tooltip("If the place has any inferior collider.")] private bool _haveColliders;
-		[SerializeField, Tooltip("If theres a follow light.")] private bool _hasFollowLight;
+		[Header( "Hidden Place" )]
+		[SerializeField, Tooltip( "Other hidden place to activate." )] private HiddenPlace _otherPlace;
+		[SerializeField, Tooltip( "The occlusion object to reveal/hide." )] private OcclusionObject _occlusionObject;
+		[SerializeField, Tooltip( "If this object will receive a signal." )] private bool _isReceptor;
+		[SerializeField, ShowIf( nameof( _isReceptor ) ), Tooltip( "The amount o time to appear/fade again after the activation." )] private float _timeToFadeAppearAgain;
+		[SerializeField, ShowIf( nameof( _isReceptor ) ), Tooltip( "If the activation of the receive signal will fade the place." )] private bool _fadeActivation;
+		[SerializeField, ShowIf( nameof( _isReceptor ) ), Tooltip( "If this place won't use his own collider." )] private bool _useOtherCollider;
+		[SerializeField, Tooltip( "If the other hidden place will appear first." )] private bool _appearFirst;
+		[SerializeField, Tooltip( "If the other hidden place will fade first." )] private bool _fadeFirst;
+		[SerializeField, Tooltip( "If this object will appear/fade instantly." )] private bool _instantly;
+		[SerializeField, Tooltip( "If the place has any inferior collider." )] private bool _haveColliders;
+		[SerializeField, Tooltip( "If theres a follow light." )] private bool _hasFollowLight;
 		private new void Awake()
 		{
 			base.Awake();
 			(_tilemap, _tilemapRenderer, _tilemapCollider) = (GetComponent<Tilemap>(), GetComponent<TilemapRenderer>(), GetComponent<TilemapCollider2D>());
 			(_selfLight, _followLight) = (GetComponent<Light2DBase>(), GetComponentInChildren<Light2DBase>());
-			_sender.SetFormat(MessageFormat.State);
-			_sender.SetAdditionalData(_occlusionObject);
+			_sender.SetFormat( MessageFormat.State );
+			_sender.SetAdditionalData( _occlusionObject );
 		}
 		private new void OnDestroy()
 		{
 			base.OnDestroy();
-			EffectsController.OffGlobalLight(_selfLight);
+			EffectsController.OffGlobalLight( _selfLight );
 		}
 		private void Start()
 		{
 			_activation = !_fadeActivation;
-			if (_isReceptor)
+			if ( _isReceptor )
 				(_tilemapRenderer.enabled, _tilemapCollider.enabled) = (_fadeActivation, _fadeActivation && !_useOtherCollider);
 		}
 		private void FixedUpdate()
 		{
-			if (_follow)
-				_followLight.transform.position = GwambaStateMarker.Localization;
+			if ( _follow )
+				_followLight.transform.position = CharacterExporter.GwambaLocalization();
 		}
-		private IEnumerator Fade(bool appear)
+		private IEnumerator Fade( bool appear )
 		{
 			bool onFirst = false;
-			if (_otherPlace)
-				if (onFirst = _otherPlace._appearFirst && _otherPlace._activation)
-					yield return StartCoroutine(_otherPlace.Fade(true));
-				else if (onFirst = _otherPlace._fadeFirst && !_otherPlace._activation)
-					yield return StartCoroutine(_otherPlace.Fade(false));
-			if (_isReceptor)
+			if ( _otherPlace )
+				if ( onFirst = _otherPlace._appearFirst && _otherPlace._activation )
+					yield return StartCoroutine( _otherPlace.Fade( true ) );
+				else if ( onFirst = _otherPlace._fadeFirst && !_otherPlace._activation )
+					yield return StartCoroutine( _otherPlace.Fade( false ) );
+			if ( _isReceptor )
 				_activation = !_activation;
-			if (appear)
-				EffectsController.OffGlobalLight(_selfLight);
+			if ( appear )
+				EffectsController.OffGlobalLight( _selfLight );
 			else
-				EffectsController.OnGlobalLight(_selfLight);
-			if (_hasFollowLight)
+				EffectsController.OnGlobalLight( _selfLight );
+			if ( _hasFollowLight )
 				_follow = !appear;
 			void Occlusion()
 			{
-				if (_occlusionObject)
+				if ( _occlusionObject )
 				{
-					_sender.SetToggle(appear);
-					_sender.Send(MessagePath.System);
+					_sender.SetToggle( appear );
+					_sender.Send( MessagePath.System );
 				}
 			}
-			IEnumerator OpacityLevel(float alpha)
+			IEnumerator OpacityLevel( float alpha )
 			{
-				yield return new WaitUntil(() => isActiveAndEnabled);
+				yield return new WaitUntil( () => isActiveAndEnabled );
 				Color color = _tilemap.color;
 				color.a = alpha;
 				_tilemap.color = color;
 			}
-			if (appear)
+			if ( appear )
 			{
 				Occlusion();
 				_tilemapRenderer.enabled = true;
-				if (_instantly)
+				if ( _instantly )
 				{
 					Color color = _tilemap.color;
 					color.a = 1F;
 					_tilemap.color = color;
 				}
 				else
-					for (float i = 0F; 1F > _tilemap.color.a; i += 1E-1F)
-						yield return OpacityLevel(i);
+					for ( float i = 0F; 1F > _tilemap.color.a; i += 1E-1F )
+						yield return OpacityLevel( i );
 			}
 			else
 			{
-				if (_instantly)
+				if ( _instantly )
 				{
 					Color color = _tilemap.color;
 					color.a = 1F;
 					_tilemap.color = color;
 				}
 				else
-					for (float i = 1F; 0F < _tilemap.color.a; i -= 1E-1F)
-						yield return OpacityLevel(i);
+					for ( float i = 1F; 0F < _tilemap.color.a; i -= 1E-1F )
+						yield return OpacityLevel( i );
 				_tilemapRenderer.enabled = false;
 				Occlusion();
 			}
-			if (_haveColliders)
+			if ( _haveColliders )
 				_tilemapCollider.enabled = appear;
-			if (_otherPlace && !onFirst)
-				if (!_otherPlace._appearFirst && _otherPlace._activation)
-					StartCoroutine(_otherPlace.Fade(true));
-				else if (!_otherPlace._fadeFirst && !_otherPlace._activation)
-					StartCoroutine(_otherPlace.Fade(false));
+			if ( _otherPlace && !onFirst )
+				if ( !_otherPlace._appearFirst && _otherPlace._activation )
+					StartCoroutine( _otherPlace.Fade( true ) );
+				else if ( !_otherPlace._fadeFirst && !_otherPlace._activation )
+					StartCoroutine( _otherPlace.Fade( false ) );
 		}
-		private void OnTriggerEnter2D(Collider2D other)
+		private void OnTriggerEnter2D( Collider2D other )
 		{
-			if (!_isReceptor && GwambaStateMarker.EqualObject(other.gameObject))
-				StartCoroutine(Fade(false));
+			if ( !_isReceptor && CharacterExporter.EqualGwamba( other.gameObject ) )
+				StartCoroutine( Fade( false ) );
 		}
-		private void OnTriggerExit2D(Collider2D other)
+		private void OnTriggerExit2D( Collider2D other )
 		{
-			if (!_isReceptor && GwambaStateMarker.EqualObject(other.gameObject))
-				StartCoroutine(Fade(true));
+			if ( !_isReceptor && CharacterExporter.EqualGwamba( other.gameObject ) )
+				StartCoroutine( Fade( true ) );
 		}
 		public void Execute()
 		{
-			if (0F < _timeToFadeAppearAgain)
-				StartCoroutine(FadeTimed(_activation));
+			if ( 0F < _timeToFadeAppearAgain )
+				StartCoroutine( FadeTimed( _activation ) );
 			else
-				StartCoroutine(Fade(_activation));
-			IEnumerator FadeTimed(bool appear)
+				StartCoroutine( Fade( _activation ) );
+			IEnumerator FadeTimed( bool appear )
 			{
-				yield return Fade(appear);
-				yield return new WaitTime(this, _timeToFadeAppearAgain, true);
-				StartCoroutine(Fade(!appear));
+				yield return Fade( appear );
+				yield return new WaitTime( this, _timeToFadeAppearAgain, true );
+				StartCoroutine( Fade( !appear ) );
 			}
 		}
 	};
