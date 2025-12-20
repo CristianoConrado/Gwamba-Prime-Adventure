3b9154efad5c425c19bef20b5793c6f62cf01c91
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 19:12:13 -0300

    Update Out Of Bounds.cs

--- System/Out Of Bounds.cs
+++ System/Out Of Bounds.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 namespace GwambaPrimeAdventure
 {
-	[DisallowMultipleComponent, Icon(WorldBuild.PROJECT_ICON), RequireComponent(typeof(Transform), typeof(Collider2D))]
-    internal sealed class OutOfBounds : MonoBehaviour
-    {
-		private void OnTriggerEnter2D(Collider2D collision) => Destroy(collision.gameObject);
+	[DisallowMultipleComponent, Icon( WorldBuild.PROJECT_ICON ), RequireComponent( typeof( Transform ), typeof( Collider2D ) )]
+	internal sealed class OutOfBounds : MonoBehaviour
+	{
+		private void OnTriggerEnter2D( Collider2D collision ) => Destroy( collision.gameObject );
 	};
 };
