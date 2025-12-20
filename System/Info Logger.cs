6a0a1818be7848ca11350dd11c743eccd1557009
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 19:11:00 -0300

    Update Info Logger.cs

--- System/Info Logger.cs
+++ System/Info Logger.cs
@@ -1,4 +1,4 @@
-﻿#if UNITY_EDITOR
+#if UNITY_EDITOR
 using UnityEngine;
 using System.IO;
 using System.Runtime.CompilerServices;
@@ -6,8 +6,8 @@ namespace GwambaPrimeAdventure
 {
 	public interface IInfoLogger
 	{
-		public void LogInfo(object message, [CallerMemberName] string member = "", [CallerFilePath] string file = "", [CallerLineNumber] int line = 0)
-			=> Debug.Log($"[{Path.GetFileName(file)} : {line} - {member}] " + message);
+		public void LogInfo( object message, [CallerMemberName] string member = "", [CallerFilePath] string file = "", [CallerLineNumber] int line = 0 )
+			=> Debug.Log( $"[{Path.GetFileName( file )} : {line} - {member}] " + message );
 	};
 	public sealed class InfoLogger : IInfoLogger
 	{
