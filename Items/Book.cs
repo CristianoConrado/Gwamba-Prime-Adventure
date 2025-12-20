4fe05e2e3a74e1f0453f9db0e53a1d973b4f33ae
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:17:42 -0300

    Update Book.cs

--- Items/Book.cs
+++ Items/Book.cs
@@ -3,32 +3,32 @@ using System.Collections;
 using GwambaPrimeAdventure.Connection;
 namespace GwambaPrimeAdventure.Item
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(SpriteRenderer), typeof(BoxCollider2D))]
+	[DisallowMultipleComponent, RequireComponent( typeof( Transform ), typeof( SpriteRenderer ), typeof( BoxCollider2D ) )]
 	internal sealed class Book : StateController, ILoader, ICollectable
 	{
-		[Header("Conditions")]
-		[SerializeField, Tooltip("The sprite to show when the book gor cacthed.")] private Sprite _bookCacthed;
+		[Header( "Conditions" )]
+		[SerializeField, Tooltip( "The sprite to show when the book gor cacthed." )] private Sprite _bookCacthed;
 		public IEnumerator Load()
 		{
-			SaveController.Load(out SaveFile saveFile);
-			if (saveFile.Books.ContainsKey(name))
+			SaveController.Load( out SaveFile saveFile );
+			if ( saveFile.Books.ContainsKey( name ) )
 			{
-				if (saveFile.Books[name])
+				if ( saveFile.Books[ name ] )
 					GetComponent<SpriteRenderer>().sprite = _bookCacthed;
 				yield break;
 			}
-			saveFile.Books.Add(name, false);
+			saveFile.Books.Add( name, false );
 			yield return null;
 		}
 		public void Collect()
 		{
-			SaveController.Load(out SaveFile saveFile);
-			if (!saveFile.Books[name])
-				saveFile.Books[name] = true;
+			SaveController.Load( out SaveFile saveFile );
+			if ( !saveFile.Books[ name ] )
+				saveFile.Books[ name ] = true;
 			GetComponent<SpriteRenderer>().sprite = _bookCacthed;
-			if (!saveFile.GeneralObjects.Contains(name))
-				saveFile.GeneralObjects.Add(name);
-			SaveController.WriteSave(saveFile);
+			if ( !saveFile.GeneralObjects.Contains( name ) )
+				saveFile.GeneralObjects.Add( name );
+			SaveController.WriteSave( saveFile );
 		}
 	};
 };
