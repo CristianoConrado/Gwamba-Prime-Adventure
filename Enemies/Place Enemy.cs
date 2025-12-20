cb51658149a557de15d02a4fd3fec64169b4e978
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:57:43 -0300

    Update Place Enemy.cs

--- Enemies/Place Enemy.cs
+++ Enemies/Place Enemy.cs
@@ -3,54 +3,54 @@ using UnityEngine.Tilemaps;
 using System.Collections;
 namespace GwambaPrimeAdventure.Enemy
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Tilemap), typeof(TilemapRenderer), typeof(TilemapCollider2D)), RequireComponent(typeof(CompositeCollider2D))]
+	[DisallowMultipleComponent, RequireComponent( typeof( Tilemap ), typeof( TilemapRenderer ), typeof( TilemapCollider2D ) ), RequireComponent( typeof( CompositeCollider2D ) )]
 	internal sealed class PlaceEnemy : EnemyProvider, IConnector
 	{
 		private Tilemap _tilemap;
 		private TilemapCollider2D _tilemapCollider;
 		private IEnumerator _appearFadeEvent;
-		[Header("Interactions")]
-		[SerializeField, Tooltip("If anything can be hurt.")] private bool _hurtEveryone;
-		[SerializeField, Tooltip("If this enemy will react to any damage taken.")] private bool _reactToDamage;
+		[Header( "Interactions" )]
+		[SerializeField, Tooltip( "If anything can be hurt." )] private bool _hurtEveryone;
+		[SerializeField, Tooltip( "If this enemy will react to any damage taken." )] private bool _reactToDamage;
 		private new void Awake()
 		{
 			base.Awake();
 			(_tilemap, _tilemapCollider) = (GetComponent<Tilemap>(), GetComponent<TilemapCollider2D>());
-			Sender.Include(this);
+			Sender.Include( this );
 		}
 		private new void OnDestroy()
 		{
 			base.OnDestroy();
-			if (_appearFadeEvent is not null)
+			if ( _appearFadeEvent is not null )
 				_appearFadeEvent = null;
-			Sender.Exclude(this);
+			Sender.Exclude( this );
 		}
 		private void Update() => _appearFadeEvent?.MoveNext();
-		public void Receive(MessageData message)
+		public void Receive( MessageData message )
 		{
-			if (message.AdditionalData is not null && message.AdditionalData is EnemyProvider[] enemies && 0 < enemies.Length)
-				foreach (EnemyProvider enemy in enemies)
-					if (enemy && this == enemy)
+			if ( message.AdditionalData is not null && message.AdditionalData is EnemyProvider[] enemies && 0 < enemies.Length )
+				foreach ( EnemyProvider enemy in enemies )
+					if ( enemy && this == enemy )
 					{
-						if (MessageFormat.State == message.Format && message.ToggleValue.HasValue)
-							_appearFadeEvent = AppearFade(message.ToggleValue.Value);
-						else if (MessageFormat.Event == message.Format && _reactToDamage)
-							_appearFadeEvent = AppearFade(0F >= _tilemap.color.a);
-						IEnumerator AppearFade(bool appear)
+						if ( MessageFormat.State == message.Format && message.ToggleValue.HasValue )
+							_appearFadeEvent = AppearFade( message.ToggleValue.Value );
+						else if ( MessageFormat.Event == message.Format && _reactToDamage )
+							_appearFadeEvent = AppearFade( 0F >= _tilemap.color.a );
+						IEnumerator AppearFade( bool appear )
 						{
 							Color color = _tilemap.color;
-							if (appear)
-								for (float i = 0F; 1F > _tilemap.color.a; i += 1E-1F)
+							if ( appear )
+								for ( float i = 0F; 1F > _tilemap.color.a; i += 1E-1F )
 								{
-									yield return new WaitUntil(() => isActiveAndEnabled && !IsStunned);
+									yield return new WaitUntil( () => isActiveAndEnabled && !IsStunned );
 									color.a = i;
 									_tilemap.color = color;
 									yield return null;
 								}
 							else
-								for (float i = 1F; 0F < _tilemap.color.a; i -= 1E-1F)
+								for ( float i = 1F; 0F < _tilemap.color.a; i -= 1E-1F )
 								{
-									yield return new WaitUntil(() => isActiveAndEnabled && !IsStunned);
+									yield return new WaitUntil( () => isActiveAndEnabled && !IsStunned );
 									color.a = i;
 									_tilemap.color = color;
 									yield return null;
