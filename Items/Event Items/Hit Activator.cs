332d3d615a26e7779159d1ce9cc3a3cb5fa37028
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:10:46 -0300

    Update Hit Activator.cs

--- Items/Event Items/Hit Activator.cs
+++ Items/Event Items/Hit Activator.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 namespace GwambaPrimeAdventure.Item.EventItem
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Collider2D))]
+	[DisallowMultipleComponent, RequireComponent( typeof( Collider2D ) )]
 	internal sealed class HitActivator : Activator, IDestructible
 	{
-		[Header("Hit Activator")]
-		[SerializeField, Tooltip("The amount of damage that this object have to receive real damage.")] private ushort _biggerDamage;
+		[Header( "Hit Activator" )]
+		[SerializeField, Tooltip( "The amount of damage that this object have to receive real damage." )] private ushort _biggerDamage;
 		public short Health => 0;
-		public bool Hurt(ushort damage)
+		public bool Hurt( ushort damage )
 		{
-			if (damage >= _biggerDamage && Usable)
+			if ( damage >= _biggerDamage && Usable )
 				Activation();
 			return damage >= _biggerDamage && Usable;
 		}
-		public void Stun(ushort stunStength, float stunTime) { }
+		public void Stun( ushort stunStength, float stunTime ) { }
 	};
 };
