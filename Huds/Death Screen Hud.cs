d84477fd22cf702276d0f51ea9570fda7f311549
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:23:46 -0300

    Update Death Screen Hud.cs

--- Huds/Death Screen Hud.cs
+++ Huds/Death Screen Hud.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 namespace GwambaPrimeAdventure.Hud
 {
-	[DisallowMultipleComponent, Icon(WorldBuild.PROJECT_ICON), RequireComponent(typeof(Transform), typeof(UIDocument))]
+	[DisallowMultipleComponent, Icon( WorldBuild.PROJECT_ICON ), RequireComponent( typeof( Transform ), typeof( UIDocument ) )]
 	internal sealed class DeathScreenHud : MonoBehaviour
 	{
 		private static DeathScreenHud _instance;
@@ -14,14 +14,14 @@ namespace GwambaPrimeAdventure.Hud
 		internal Button GameOver { get; private set; }
 		private void Awake()
 		{
-			if (_instance)
+			if ( _instance )
 			{
-				Destroy(gameObject, WorldBuild.MINIMUM_TIME_SPACE_LIMIT);
+				Destroy( gameObject, WorldBuild.MINIMUM_TIME_SPACE_LIMIT );
 				return;
 			}
-			(_instance, RootElement) = (this, GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>(nameof(RootElement)));
-			(Curtain, Text, Continue) = (RootElement.Q<VisualElement>(nameof(Curtain)), RootElement.Q<Label>(nameof(Text)), RootElement.Q<Button>(nameof(Continue)));
-			(OutLevel, GameOver) = (RootElement.Q<Button>(nameof(OutLevel)), RootElement.Q<Button>(nameof(GameOver)));
+			(_instance, RootElement) = (this, GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>( nameof( RootElement ) ));
+			(Curtain, Text, Continue) = (RootElement.Q<VisualElement>( nameof( Curtain ) ), RootElement.Q<Label>( nameof( Text ) ), RootElement.Q<Button>( nameof( Continue ) ));
+			(OutLevel, GameOver) = (RootElement.Q<Button>( nameof( OutLevel ) ), RootElement.Q<Button>( nameof( GameOver ) ));
 		}
 	};
 };
