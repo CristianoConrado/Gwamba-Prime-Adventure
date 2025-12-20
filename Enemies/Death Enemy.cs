b934d1fa5db982b740fdb88535841792bdb9dba4
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:56:08 -0300

    Update Death Enemy.cs

--- Enemies/Death Enemy.cs
+++ Enemies/Death Enemy.cs
@@ -5,46 +5,46 @@ namespace GwambaPrimeAdventure.Enemy
 	[DisallowMultipleComponent]
 	internal sealed class DeathEnemy : EnemyProvider, IDestructible
 	{
-		private bool _isDead = false;
 		private float _deathTime = 0F;
-		[Header("Death Enemy")]
-		[SerializeField, Tooltip("The death statitics of this enemy.")] private DeathStatistics _statistics;
+		private bool _isDead = false;
+		[Header( "Death Enemy" )]
+		[SerializeField, Tooltip( "The death statitics of this enemy." )] private DeathStatistics _statistics;
 		private void Update()
 		{
-			if (_isDead)
-				if (0F >= (_deathTime -= Time.deltaTime))
+			if ( _isDead )
+				if ( 0F >= ( _deathTime -= Time.deltaTime ) )
 				{
 					_isDead = false;
-					if (_statistics.ChildEnemy)
-						Instantiate(_statistics.ChildEnemy, _statistics.SpawnPoint, Quaternion.identity).transform.SetParent(null);
-					if (_statistics.ChildProjectile)
-						Instantiate(_statistics.ChildProjectile, _statistics.SpawnPoint, Quaternion.identity).transform.SetParent(null);
-					Destroy(gameObject);
+					if ( _statistics.ChildEnemy )
+						Instantiate( _statistics.ChildEnemy, _statistics.SpawnPoint, Quaternion.identity ).transform.SetParent( null );
+					if ( _statistics.ChildProjectile )
+						Instantiate( _statistics.ChildProjectile, _statistics.SpawnPoint, Quaternion.identity ).transform.SetParent( null );
+					Destroy( gameObject );
 				}
 		}
-		private void OnTriggerEnter2D(Collider2D other)
+		private void OnTriggerEnter2D( Collider2D other )
 		{
-			if (!_isDead && _statistics.OnTouch && other.TryGetComponent<IDestructible>(out _))
+			if ( !_isDead && _statistics.OnTouch && other.TryGetComponent<IDestructible>( out _ ) )
 			{
-				_sender.SetFormat(MessageFormat.State);
-				_sender.SetToggle(false);
-				_sender.Send(MessagePath.Enemy);
+				_sender.SetFormat( MessageFormat.State );
+				_sender.SetToggle( false );
+				_sender.Send( MessagePath.Enemy );
 				(_deathTime, _isDead) = (_statistics.TimeToDie, true);
 			}
 		}
-		public new bool Hurt(ushort damage)
+		public new bool Hurt( ushort damage )
 		{
-			if (_isDead)
+			if ( _isDead )
 				return false;
-			if (0 >= Health - (short)damage)
+			if ( 0 >= Health - (short) damage )
 			{
-				_sender.SetFormat(MessageFormat.State);
-				_sender.SetToggle(false);
-				_sender.Send(MessagePath.Enemy);
+				_sender.SetFormat( MessageFormat.State );
+				_sender.SetToggle( false );
+				_sender.Send( MessagePath.Enemy );
 				(_deathTime, _isDead) = (_statistics.TimeToDie, true);
 				return true;
 			}
-			return base.Hurt(damage);
+			return base.Hurt( damage );
 		}
 	};
 };
