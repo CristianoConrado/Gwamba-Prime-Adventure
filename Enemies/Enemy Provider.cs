42c37d61745129187af6aab6eeaf4b6f30017687
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:56:57 -0300

    Update Enemy Provider.cs

--- Enemies/Enemy Provider.cs
+++ Enemies/Enemy Provider.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 namespace GwambaPrimeAdventure.Enemy
 {
-	[RequireComponent(typeof(Transform), typeof(EnemyController), typeof(Collider2D))]
+	[RequireComponent( typeof( Transform ), typeof( EnemyController ), typeof( Collider2D ) )]
 	internal abstract class EnemyProvider : StateController, IDestructible
 	{
 		protected EnemyController _controller;
 		protected Collider2D _collider;
 		protected readonly Sender _sender = Sender.Create();
 		protected bool _stopWorking = false;
-		[Header("Enemy Provider")]
-		[SerializeField, Tooltip("The enemies to send messages.")] private EnemyProvider[] _enemiesToSend;
-		[SerializeField, Tooltip("The level of priority to use the destructible side.")] private ushort _destructilbePriority = 0;
+		[Header( "Enemy Provider" )]
+		[SerializeField, Tooltip( "The enemies to send messages." )] private EnemyProvider[] _enemiesToSend;
+		[SerializeField, Tooltip( "The level of priority to use the destructible side." )] private ushort _destructilbePriority = 0;
 		protected Rigidbody2D Rigidbody => _controller.Rigidbody;
 		public MessagePath Path => MessagePath.Enemy;
 		protected bool IsStunned => _controller.IsStunned;
@@ -20,32 +20,32 @@ namespace GwambaPrimeAdventure.Enemy
 		{
 			base.Awake();
 			(_controller, _collider) = (GetComponent<EnemyController>(), GetComponent<Collider2D>());
-			_sender.SetAdditionalData(_enemiesToSend);
+			_sender.SetAdditionalData( _enemiesToSend );
 		}
-		public bool Hurt(ushort damage)
+		public bool Hurt( ushort damage )
 		{
-			if (_controller.ProvidenceStatistics.ReactToDamage)
+			if ( _controller.ProvidenceStatistics.ReactToDamage )
 			{
-				if (_controller.ProvidenceStatistics.HasIndex)
-					_sender.SetNumber(_controller.ProvidenceStatistics.IndexEvent);
-				_sender.SetFormat(MessageFormat.Event);
-				_sender.Send(MessagePath.Enemy);
+				if ( _controller.ProvidenceStatistics.HasIndex )
+					_sender.SetNumber( _controller.ProvidenceStatistics.IndexEvent );
+				_sender.SetFormat( MessageFormat.Event );
+				_sender.Send( MessagePath.Enemy );
 			}
-			if (0 >= (_controller.Vitality -= (short)damage))
-				Destroy(gameObject);
+			if ( 0 >= ( _controller.Vitality -= (short) damage ) )
+				Destroy( gameObject );
 			return true;
 		}
-		public void Stun(ushort stunStength, float stunTime)
+		public void Stun( ushort stunStength, float stunTime )
 		{
-			if (_controller.IsStunned = !_controller.ProvidenceStatistics.NoHitStun)
+			if ( _controller.IsStunned = !_controller.ProvidenceStatistics.NoHitStun )
 			{
 				_controller.OnDisable();
 				_controller.StunTimer = stunTime;
 			}
-			if (0 >= (_controller.ArmorResistance -= (short)stunStength))
+			if ( 0 >= ( _controller.ArmorResistance -= (short) stunStength ) )
 			{
 				_controller.OnDisable();
-				(_controller.StunTimer, _controller.ArmorResistance) = (_controller.ProvidenceStatistics.StunnedTime, (short)_controller.ProvidenceStatistics.HitResistance);
+				(_controller.StunTimer, _controller.ArmorResistance) = (_controller.ProvidenceStatistics.StunnedTime, (short) _controller.ProvidenceStatistics.HitResistance);
 			}
 		}
 	};
