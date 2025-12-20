6194468735919feaf9ba072eeaa45488e1474dac
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 17:04:15 -0300

    Add files via upload

---
+++ Characters/Character Exporter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+namespace GwambaPrimeAdventure.Character
+{
+    public static class CharacterExporter
+    {
+		public static Vector2 GwambaLocalization() => GwambaState<GwambaMarker>.Instance ? GwambaState<GwambaMarker>.Instance.transform.position : Vector2.zero;
+		public static bool EqualGwamba( params GameObject[] others ) => GwambaState<GwambaMarker>.Instance ? GwambaState<GwambaMarker>.Instance.EqualObject( others ) : false;
+	};
+};
