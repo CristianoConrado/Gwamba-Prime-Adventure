6bfadcf49f4c734bfdcc5b51ddcdfc87ba196c3f
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:19:47 -0300

    Update Progress Obstacle.cs

--- Items/Progress Obstacle.cs
+++ Items/Progress Obstacle.cs
@@ -3,24 +3,24 @@ using System.Collections;
 using GwambaPrimeAdventure.Connection;
 namespace GwambaPrimeAdventure.Item
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
+	[DisallowMultipleComponent, RequireComponent( typeof( Transform ), typeof( Collider2D ) )]
 	internal sealed class ProgressObstacle : StateController, ILoader
 	{
-		[Header("Progress Interactions")]
-		[SerializeField, Tooltip("The index that this object will check if theres anything completed.")] private ushort _progressIndex;
-		[SerializeField, Tooltip("If the index is about the boss.")] private bool _isBossProgress;
-		[SerializeField, Tooltip("If this object will be saved as already existent object.")] private bool _saveOnSpecifics;
+		[Header( "Progress Interactions" )]
+		[SerializeField, Tooltip( "The index that this object will check if theres anything completed." )] private ushort _progressIndex;
+		[SerializeField, Tooltip( "If the index is about the boss." )] private bool _isBossProgress;
+		[SerializeField, Tooltip( "If this object will be saved as already existent object." )] private bool _saveOnSpecifics;
 		public IEnumerator Load()
 		{
-			SaveController.Load(out SaveFile saveFile);
-			if (_isBossProgress ? saveFile.DeafetedBosses[_progressIndex - 1] : saveFile.LevelsCompleted[_progressIndex - 1])
+			SaveController.Load( out SaveFile saveFile );
+			if ( _isBossProgress ? saveFile.DeafetedBosses[ _progressIndex - 1 ] : saveFile.LevelsCompleted[ _progressIndex - 1 ] )
 			{
-				if (_saveOnSpecifics && !saveFile.GeneralObjects.Contains(name))
+				if ( _saveOnSpecifics && !saveFile.GeneralObjects.Contains( name ) )
 				{
-					saveFile.GeneralObjects.Add(name);
-					SaveController.WriteSave(saveFile);
+					saveFile.GeneralObjects.Add( name );
+					SaveController.WriteSave( saveFile );
 				}
-				Destroy(gameObject);
+				Destroy( gameObject );
 			}
 			yield return null;
 		}
