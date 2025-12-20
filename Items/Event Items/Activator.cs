b0c1dd2cb0113653c922331a2d8270c92bc00c8a
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:09:55 -0300

    Update Activator.cs

--- Items/Event Items/Activator.cs
+++ Items/Event Items/Activator.cs
@@ -3,21 +3,21 @@ using System.Collections;
 using GwambaPrimeAdventure.Connection;
 namespace GwambaPrimeAdventure.Item.EventItem
 {
-	[RequireComponent(typeof(Transform))]
+	[RequireComponent( typeof( Transform ) )]
 	internal abstract class Activator : StateController, ILoader
 	{
 		private Animator _animator;
 		private readonly int
-			IsOn = Animator.StringToHash(nameof(IsOn)),
-			Use = Animator.StringToHash(nameof(Use)),
-			UseAgain = Animator.StringToHash(nameof(UseAgain));
+			IsOn = Animator.StringToHash( nameof( IsOn ) ),
+			Use = Animator.StringToHash( nameof( Use ) ),
+			UseAgain = Animator.StringToHash( nameof( UseAgain ) );
 		private bool
 			_used = false,
 			_usedOne = false,
 			_usable = true;
-		[Header("Activator")]
-		[SerializeField, Tooltip("The activator only can be activeted one time.")] private bool _oneActivation;
-		[SerializeField, Tooltip("If this object have been activeted before it will always be activeted.")] private bool _saveOnSpecifics;
+		[Header( "Activator" )]
+		[SerializeField, Tooltip( "The activator only can be activeted one time." )] private bool _oneActivation;
+		[SerializeField, Tooltip( "If this object have been activeted before it will always be activeted." )] private bool _saveOnSpecifics;
 		protected bool Usable => _usable;
 		protected new void Awake()
 		{
@@ -26,40 +26,40 @@ namespace GwambaPrimeAdventure.Item.EventItem
 		}
 		protected void OnEnable()
 		{
-			if (_animator)
-				_animator.SetFloat(IsOn, 1F);
+			if ( _animator )
+				_animator.SetFloat( IsOn, 1F );
 		}
 		protected void OnDisable()
 		{
-			if (_animator)
-				_animator.SetFloat(IsOn, 0F);
+			if ( _animator )
+				_animator.SetFloat( IsOn, 0F );
 		}
 		public IEnumerator Load()
 		{
-			SaveController.Load(out SaveFile saveFile);
-			if (_saveOnSpecifics && saveFile.GeneralObjects.Contains(name))
+			SaveController.Load( out SaveFile saveFile );
+			if ( _saveOnSpecifics && saveFile.GeneralObjects.Contains( name ) )
 				Activation();
 			yield return null;
 		}
 		protected void Activation()
 		{
-			if (_oneActivation && _usedOne)
+			if ( _oneActivation && _usedOne )
 				return;
 			_used = !_used;
-			if (_oneActivation)
+			if ( _oneActivation )
 				_usable = false;
-			if (_animator)
-				if (_used)
-					_animator.SetTrigger(Use);
+			if ( _animator )
+				if ( _used )
+					_animator.SetTrigger( Use );
 				else
-					_animator.SetTrigger(UseAgain);
+					_animator.SetTrigger( UseAgain );
 			_usedOne = true;
-			Receptor.ReceiveSignal(this);
-			SaveController.Load(out SaveFile saveFile);
-			if (_saveOnSpecifics && !saveFile.GeneralObjects.Contains(name))
+			Receptor.ReceiveSignal( this );
+			SaveController.Load( out SaveFile saveFile );
+			if ( _saveOnSpecifics && !saveFile.GeneralObjects.Contains( name ) )
 			{
-				saveFile.GeneralObjects.Add(name);
-				SaveController.WriteSave(saveFile);
+				saveFile.GeneralObjects.Add( name );
+				SaveController.WriteSave( saveFile );
 			}
 		}
 	};
