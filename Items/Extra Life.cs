ee0359c27bcfcce5bf30f2f6eea5717f14e9b528
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:18:06 -0300

    Update Extra Life.cs

--- Items/Extra Life.cs
+++ Items/Extra Life.cs
@@ -3,28 +3,28 @@ using System.Collections;
 using GwambaPrimeAdventure.Connection;
 namespace GwambaPrimeAdventure.Item
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(SpriteRenderer), typeof(BoxCollider2D))]
+	[DisallowMultipleComponent, RequireComponent( typeof( Transform ), typeof( SpriteRenderer ), typeof( BoxCollider2D ) )]
 	internal sealed class ExtraLife : StateController, ILoader, ICollectable
 	{
-		[Header("Condition")]
-		[SerializeField, Tooltip("If this object will be saved as already existent object.")] private bool _saveOnSpecifics;
+		[Header( "Condition" )]
+		[SerializeField, Tooltip( "If this object will be saved as already existent object." )] private bool _saveOnSpecifics;
 		public IEnumerator Load()
 		{
-			SaveController.Load(out SaveFile saveFile);
-			if (saveFile.LifesAcquired.Contains(name))
-				Destroy(gameObject);
+			SaveController.Load( out SaveFile saveFile );
+			if ( saveFile.LifesAcquired.Contains( name ) )
+				Destroy( gameObject );
 			yield return null;
 		}
 		public void Collect()
 		{
-			SaveController.Load(out SaveFile saveFile);
-			if (100 > saveFile.Lifes)
+			SaveController.Load( out SaveFile saveFile );
+			if ( 100 > saveFile.Lifes )
 				saveFile.Lifes += 1;
-			saveFile.LifesAcquired.Add(name);
-			if (_saveOnSpecifics && !saveFile.GeneralObjects.Contains(name))
-				saveFile.GeneralObjects.Add(name);
-			SaveController.WriteSave(saveFile);
-			Destroy(gameObject);
+			saveFile.LifesAcquired.Add( name );
+			if ( _saveOnSpecifics && !saveFile.GeneralObjects.Contains( name ) )
+				saveFile.GeneralObjects.Add( name );
+			SaveController.WriteSave( saveFile );
+			Destroy( gameObject );
 		}
 	};
 };
