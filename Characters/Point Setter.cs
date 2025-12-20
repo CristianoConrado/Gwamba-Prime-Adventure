685b00124f03800a1aeff7b8f491afeb5c133dc8
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 17:32:43 -0300

    Update Point Setter.cs

--- Characters/Point Setter.cs
+++ Characters/Point Setter.cs
@@ -6,24 +6,24 @@ namespace GwambaPrimeAdventure.Character
 	[DisallowMultipleComponent, RequireComponent( typeof( Transform ), typeof( BoxCollider2D ) )]
 	internal sealed class PointSetter : StateController, ILoader
 	{
-		private static PointSetter _instance;
+		private static PointSetter Instance;
 		[SerializeField, Tooltip( "The name of the hubby world scene." ), Space( WorldBuild.FIELD_SPACE_LENGTH * 2F )] private SceneField _hubbyWorldScene;
 		[SerializeField, Tooltip( "If this point is faced to left." )] private bool _turnToLeft;
 		[SerializeField, Tooltip( "Which point setter is setted when scene is the hubby world." )] private ushort _selfIndex;
-		internal static Vector2 CheckedPoint => _instance ? _instance.transform.position : Vector2.zero;
-		internal static bool TurnToLeft => _instance ? _instance._turnToLeft : false;
+		internal static Vector2 CheckedPoint => Instance ? Instance.transform.position : Vector2.zero;
+		internal static bool TurnToLeft => Instance ? Instance._turnToLeft : false;
 		public IEnumerator Load()
 		{
 			SaveController.Load( out SaveFile saveFile );
 			if ( gameObject.scene.name == _hubbyWorldScene && !string.IsNullOrEmpty( saveFile.LastLevelEntered ) )
 				if ( saveFile.LastLevelEntered.Contains( $"{_selfIndex}" ) )
-					_instance = this;
+					Instance = this;
 			yield return null;
 		}
 		private void OnTriggerEnter2D( Collider2D other )
 		{
-			if ( GwambaState.EqualObject( other.gameObject ) && this != _instance )
-				_instance = this;
+			if ( GwambaState<GwambaMarker>.Instance.EqualObject( other.gameObject ) && this != Instance )
+				Instance = this;
 		}
 	};
 };
