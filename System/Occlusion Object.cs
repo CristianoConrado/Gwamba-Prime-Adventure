14a19743696495f4e1df5ea84bafe2c2ddadb6b8
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 19:12:02 -0300

    Update Occlusion Object.cs

--- System/Occlusion Object.cs
+++ System/Occlusion Object.cs
@@ -3,41 +3,41 @@ using System.Collections;
 using System.Linq;
 namespace GwambaPrimeAdventure
 {
-	[DisallowMultipleComponent, Icon(WorldBuild.PROJECT_ICON), RequireComponent(typeof(Transform), typeof(BoxCollider2D))]
+	[DisallowMultipleComponent, Icon( WorldBuild.PROJECT_ICON ), RequireComponent( typeof( Transform ), typeof( BoxCollider2D ) )]
 	public sealed class OcclusionObject : MonoBehaviour, IConnector
 	{
-		[Header("Interactions")]
-		[SerializeField, Tooltip("If this object will activate the children.")] private bool _initialActive;
-		[SerializeField, Tooltip("If this object will turn off the collisions.")] private bool _offCollision;
-		[SerializeField, Tooltip("If this object will occlude any other object that enter the collision.")] private bool _collisionOcclusion = true;
+		[Header( "Interactions" )]
+		[SerializeField, Tooltip( "If this object will activate the children." )] private bool _initialActive;
+		[SerializeField, Tooltip( "If this object will turn off the collisions." )] private bool _offCollision;
+		[SerializeField, Tooltip( "If this object will occlude any other object that enter the collision." )] private bool _collisionOcclusion = true;
 		public MessagePath Path => MessagePath.System;
 		private void Awake()
 		{
 			GetComponent<BoxCollider2D>().enabled = !_offCollision;
-			Sender.Include(this);
+			Sender.Include( this );
 		}
-		private void OnDestroy() => Sender.Exclude(this);
+		private void OnDestroy() => Sender.Exclude( this );
 		private IEnumerator Start()
 		{
-			yield return new WaitWhile(() => SceneInitiator.IsInTrancision());
-			StateController[] states = GetComponentsInChildren<StateController>(true);
-			yield return new WaitUntil(() => states.All(state => state && state.enabled));
-			Execution(_initialActive);
+			yield return new WaitWhile( () => SceneInitiator.IsInTrancision() );
+			StateController[] states = GetComponentsInChildren<StateController>( true );
+			yield return new WaitUntil( () => states.All( state => state && state.enabled ) );
+			Execution( _initialActive );
 		}
-		internal void Execution(bool activate)
+		internal void Execution( bool activate )
 		{
-			foreach (Transform child in transform)
-				child.gameObject.SetActive(activate);
+			foreach ( Transform child in transform )
+				child.gameObject.SetActive( activate );
 		}
-		private void OnTriggerEnter2D(Collider2D other)
+		private void OnTriggerEnter2D( Collider2D other )
 		{
-			if (_collisionOcclusion && other.TryGetComponent<IOccludee>(out var occludee) && occludee.Occlude && !other.transform.parent)
-				other.transform.SetParent(transform);
+			if ( _collisionOcclusion && other.TryGetComponent<IOccludee>( out var occludee ) && occludee.Occlude && !other.transform.parent )
+				other.transform.SetParent( transform );
 		}
-		public void Receive(MessageData message)
+		public void Receive( MessageData message )
 		{
-			if (this == message.AdditionalData as OcclusionObject && MessageFormat.State == message.Format && message.ToggleValue.HasValue)
-				Execution(message.ToggleValue.Value);
+			if ( this == message.AdditionalData as OcclusionObject && MessageFormat.State == message.Format && message.ToggleValue.HasValue )
+				Execution( message.ToggleValue.Value );
 		}
 	};
 };
