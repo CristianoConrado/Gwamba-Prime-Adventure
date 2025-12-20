13551ed1d56977b6004bbc593d636082236fd575
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:10:22 -0300

    Update Destructive Object.cs

--- Items/Event Items/Destructive Object.cs
+++ Items/Event Items/Destructive Object.cs
@@ -2,57 +2,57 @@ using UnityEngine;
 using GwambaPrimeAdventure.Connection;
 namespace GwambaPrimeAdventure.Item.EventItem
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D), typeof(Receptor))]
+	[DisallowMultipleComponent, RequireComponent( typeof( Transform ), typeof( Collider2D ), typeof( Receptor ) )]
 	internal sealed class DestructiveObject : StateController, IReceptorSignal, IDestructible
 	{
 		private readonly Sender _sender = Sender.Create();
-		[Header("Destructive Object")]
-		[SerializeField, Tooltip("If there a object that will be instantiate after the destruction of ")] private OcclusionArea _occlusionObject;
-		[SerializeField, Tooltip("The vitality of this object before it destruction.")] private short _vitality;
-		[SerializeField, Tooltip("The amount of damage that this object have to receive real damage.")] private short _biggerDamage;
-		[SerializeField, Tooltip("If this object will be destructed on collision with another object.")] private bool _destroyOnCollision;
-		[SerializeField, Tooltip("If this object will be saved as already existent object.")] private bool _saveOnSpecifics;
+		[Header( "Destructive Object" )]
+		[SerializeField, Tooltip( "If there a object that will be instantiate after the destruction of " )] private OcclusionObject _occlusionObject;
+		[SerializeField, Tooltip( "The vitality of this object before it destruction." )] private short _vitality;
+		[SerializeField, Tooltip( "The amount of damage that this object have to receive real damage." )] private short _biggerDamage;
+		[SerializeField, Tooltip( "If this object will be destructed on collision with another object." )] private bool _destroyOnCollision;
+		[SerializeField, Tooltip( "If this object will be saved as already existent object." )] private bool _saveOnSpecifics;
 		public short Health => _vitality;
 		private new void Awake()
 		{
 			base.Awake();
-			_sender.SetFormat(MessageFormat.State);
-			_sender.SetAdditionalData(_occlusionObject);
-			_sender.SetToggle(true);
+			_sender.SetFormat( MessageFormat.State );
+			_sender.SetAdditionalData( _occlusionObject );
+			_sender.SetToggle( true );
 		}
 		private void Start()
 		{
-			SaveController.Load(out SaveFile saveFile);
-			if (_saveOnSpecifics && saveFile.GeneralObjects.Contains(name))
-				Destroy(gameObject);
+			SaveController.Load( out SaveFile saveFile );
+			if ( _saveOnSpecifics && saveFile.GeneralObjects.Contains( name ) )
+				Destroy( gameObject );
 		}
 		public void Execute()
 		{
-			if (_occlusionObject)
-				_sender.Send(MessagePath.System);
-			SaveController.Load(out SaveFile saveFile);
-			if (_saveOnSpecifics && !saveFile.GeneralObjects.Contains(name))
+			if ( _occlusionObject )
+				_sender.Send( MessagePath.System );
+			SaveController.Load( out SaveFile saveFile );
+			if ( _saveOnSpecifics && !saveFile.GeneralObjects.Contains( name ) )
 			{
-				saveFile.GeneralObjects.Add(name);
-				SaveController.WriteSave(saveFile);
+				saveFile.GeneralObjects.Add( name );
+				SaveController.WriteSave( saveFile );
 			}
-			Destroy(gameObject);
+			Destroy( gameObject );
 		}
 		private void DestroyOnCollision()
 		{
-			if (_destroyOnCollision)
+			if ( _destroyOnCollision )
 				Execute();
 		}
-		private void OnCollisionEnter2D(Collision2D collision) => DestroyOnCollision();
-		private void OnTriggerEnter2D(Collider2D collision) => DestroyOnCollision();
-		public bool Hurt(ushort damage)
+		private void OnCollisionEnter2D( Collision2D collision ) => DestroyOnCollision();
+		private void OnTriggerEnter2D( Collider2D collision ) => DestroyOnCollision();
+		public bool Hurt( ushort damage )
 		{
-			if (damage < _biggerDamage || 0 >= _vitality)
+			if ( damage < _biggerDamage || 0 >= _vitality )
 				return false;
-			if (0 >= (_vitality -= (short)damage))
+			if ( 0 >= ( _vitality -= (short) damage ) )
 				Execute();
 			return true;
 		}
-		public void Stun(ushort stunStength, float stunTime) { }
+		public void Stun( ushort stunStength, float stunTime ) { }
 	};
 };
