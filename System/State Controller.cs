1cc528297419f4c780984dc4c868a0e9c6cae6e3
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 19:13:11 -0300

    Update State Controller.cs

--- System/State Controller.cs
+++ System/State Controller.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 using UnityEngine.Events;
 namespace GwambaPrimeAdventure
 {
-	[Icon(WorldBuild.PROJECT_ICON)]
+	[Icon( WorldBuild.PROJECT_ICON )]
 	public abstract class StateController : MonoBehaviour
 	{
 		private static UnityAction<bool> _setState;
 		protected void Awake() => _setState += NewState;
 		protected void OnDestroy() => _setState -= NewState;
-		private void NewState(bool state) => enabled = state;
-		public static void SetState(bool newState) => _setState?.Invoke(newState);
+		private void NewState( bool state ) => enabled = state;
+		public static void SetState( bool newState ) => _setState?.Invoke( newState );
 		protected sealed class WaitTime : CustomYieldInstruction
 		{
 			private readonly StateController _instance;
@@ -19,12 +19,12 @@ namespace GwambaPrimeAdventure
 			{
 				get
 				{
-					if (0F < _time && _instance.isActiveAndEnabled)
+					if ( 0F < _time && _instance.isActiveAndEnabled )
 						_time -= _unscaled ? Time.unscaledDeltaTime : Time.deltaTime;
 					return 0F < _time;
 				}
 			}
-			public WaitTime(StateController instance, float time, bool unscaled = false) => (_instance, _time, _unscaled) = (instance, time, unscaled);
+			public WaitTime( StateController instance, float time, bool unscaled = false ) => (_instance, _time, _unscaled) = (instance, time, unscaled);
 		};
 	};
 };
