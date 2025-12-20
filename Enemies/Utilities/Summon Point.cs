251bba593af1f964e607ab830ff894fcaf58b058
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 18:41:19 -0300

    Update Summon Point.cs

--- Enemies/Utilities/Summon Point.cs
+++ Enemies/Utilities/Summon Point.cs
@@ -2,30 +2,26 @@ using UnityEngine;
 using GwambaPrimeAdventure.Character;
 namespace GwambaPrimeAdventure.Enemy.Utility
 {
-	[DisallowMultipleComponent, RequireComponent(typeof(Transform), typeof(Collider2D))]
+	[DisallowMultipleComponent, RequireComponent( typeof( Transform ), typeof( Collider2D ) )]
 	public sealed class SummonPoint : StateController
 	{
 		private ISummoner _summoner;
 		private ushort _summonIndex;
-		[Header("Interactions")]
-		[SerializeField, Tooltip("If this point will destroy itself after use.")] private bool _destroyAfter;
-		[SerializeField, Tooltip("If this point will trigger with other object.")] private bool _hasTarget;
-		public void GetTouch(ISummoner summoner, ushort summonIndex)
+		[Header( "Interactions" )]
+		[SerializeField, Tooltip( "If this point will destroy itself after use." )] private bool _destroyAfter;
+		[SerializeField, Tooltip( "If this point will trigger with other object." )] private bool _hasTarget;
+		public void GetTouch( ISummoner summoner, ushort summonIndex ) => (_summoner, _summonIndex) = (summoner, summonIndex);
+		private void OnTriggerEnter2D( Collider2D other )
 		{
-			_summoner = summoner;
-			_summonIndex = summonIndex;
-		}
-		private void OnTriggerEnter2D(Collider2D other)
-		{
-			if (_hasTarget)
+			if ( _hasTarget )
 			{
-				if (GwambaStateMarker.EqualObject(other.gameObject))
-					_summoner.OnSummon(_summonIndex);
+				if ( CharacterExporter.EqualGwamba( other.gameObject ) )
+					_summoner.OnSummon( _summonIndex );
 			}
-			else if (other.TryGetComponent<ISummoner>(out _))
-				_summoner.OnSummon(_summonIndex);
-			if (_destroyAfter)
-				Destroy(gameObject);
+			else if ( other.TryGetComponent<ISummoner>( out _ ) )
+				_summoner.OnSummon( _summonIndex );
+			if ( _destroyAfter )
+				Destroy( gameObject );
 		}
 	};
 };
