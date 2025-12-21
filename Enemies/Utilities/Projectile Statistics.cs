d4a138c4064472f2a930b77d69056e0bd699459d
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 21:14:38 -0300

    Update Projectile Statistics.cs

--- Enemies/Utilities/Projectile Statistics.cs
+++ Enemies/Utilities/Projectile Statistics.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using NaughtyAttributes;
-namespace GwambaPrimeAdventure.Enemy.Utility
+namespace GwambaPrimeAdventure.Enemy.Supply
 {
 	[CreateAssetMenu( fileName = "Enemy Projectile", menuName = "Enemy Statistics/Projectile", order = 11 )]
 	public sealed class ProjectileStatistics : ScriptableObject
