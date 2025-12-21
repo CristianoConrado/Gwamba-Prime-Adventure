65bcae33382549c197c4705db938cd65685f18ef
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-21 12:03:48 -0300

    Update World Build.cs

--- System/World Build.cs
+++ System/World Build.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-using System.Collections.Generic;
 namespace GwambaPrimeAdventure
 {
 	public static class WorldBuild
@@ -35,30 +33,5 @@ namespace GwambaPrimeAdventure
 		public const ushort UI_SCALE_WIDTH = 1920;
 		public const ushort UI_SCALE_HEIGHt = (ushort) ( UI_SCALE_WIDTH * WIDTH_HEIGHT_PROPORTION );
 		public const ushort LEVELS_COUNT = 10;
-		public static Vector2 OrthographicToRealSize( float orthographicSize ) => new Vector2( orthographicSize * 2F * HEIGHT_WIDTH_PROPORTION, orthographicSize * 2F );
-		public static Vector2 OrthographicToScreenSize( float orthographicSize ) => OrthographicToRealSize( orthographicSize ) * PIXELS_PER_UNIT;
-		public static Resolution[] PixelPerfectResolutions()
-		{
-			List<Resolution> resolutions = new List<Resolution>();
-			for ( ushort i = 0; Screen.resolutions.Length > i; i++ )
-				if ( Screen.resolutions[ i ].width % PIXEL_PERFECT_WIDTH == 0 && Screen.resolutions[ i ].height % PIXEL_PERFECT_HEIGHT == 0 )
-					resolutions.Add( Screen.resolutions[ i ] );
-			return resolutions.ToArray();
-		}
-		public static void TurnScaleX( this Transform transform, float valueChanger ) =>
-			transform.localScale = new Vector3( Mathf.Abs( transform.localScale.x ) * valueChanger, transform.localScale.y, transform.localScale.z );
-		public static void TurnScaleX( this Transform transform, bool conditionChanger ) => TurnScaleX( transform, conditionChanger ? -1F : 1F );
-		public static bool InsideRectangle( this Vector2 pointInside, Vector2 originPoint, Vector2 sizePoint )
-		{
-			return originPoint.x + sizePoint.x / 2F >= pointInside.x && originPoint.x - sizePoint.x / 2F <= pointInside.x &&
-				originPoint.y + sizePoint.y / 2F >= pointInside.y && originPoint.y - sizePoint.y / 2F <= pointInside.y;
-		}
-		public static bool OutsideRectangle( this Vector2 pointOutside, Vector2 originPoint, Vector2 sizePoint )
-		{
-			return originPoint.x + sizePoint.x / 2F < pointOutside.x || originPoint.x - sizePoint.x / 2F > pointOutside.x ||
-				originPoint.y + sizePoint.y / 2F < pointOutside.y || originPoint.y - sizePoint.y / 2F > pointOutside.y;
-		}
-		public static bool InsideCircle( this Vector2 pointInside, Vector2 originPoint, float radius ) => Vector2.Distance( originPoint, pointInside ) < radius;
-		public static bool OutsideCircle( this Vector2 pointOutside, Vector2 originPoint, float radius ) => Vector2.Distance( originPoint, pointOutside ) > radius;
 	};
 };
