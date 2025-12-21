5dd6d28e2d4ac9b9cb45094c628cd2c57f1eb39b
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-20 21:21:37 -0300

    Update Defender Enemy.cs

--- Enemies/Defender Enemy.cs
+++ Enemies/Defender Enemy.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using System.Collections;
-using GwambaPrimeAdventure.Enemy.Utility;
+using GwambaPrimeAdventure.Enemy.Supply;
 namespace GwambaPrimeAdventure.Enemy
 {
 	[DisallowMultipleComponent]
