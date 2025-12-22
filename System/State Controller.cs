1531c824de5f3944a44fe66b3a8e928b0a403b32
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-22 16:30:14 -0300

    Update State Controller.cs

--- System/State Controller.cs
+++ System/State Controller.cs
@@ -10,21 +10,5 @@ namespace GwambaPrimeAdventure
 		protected void OnDestroy() => _setState -= NewState;
 		private void NewState( bool state ) => enabled = state;
 		public static void SetState( bool newState ) => _setState?.Invoke( newState );
-		protected sealed class WaitTime : CustomYieldInstruction
-		{
-			private readonly StateController _instance;
-			private readonly bool _unscaled;
-			private float _time;
-			public override bool keepWaiting
-			{
-				get
-				{
-					if ( 0F < _time && _instance.isActiveAndEnabled )
-						_time -= _unscaled ? Time.unscaledDeltaTime : Time.deltaTime;
-					return 0F < _time;
-				}
-			}
-			public WaitTime( StateController instance, float time, bool unscaled = false ) => (_instance, _time, _unscaled) = (instance, time, unscaled);
-		};
 	};
 };
