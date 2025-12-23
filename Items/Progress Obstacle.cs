72b9f35732fbefb08ad02210ece026f7732d3770
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-22 21:35:23 -0300

    Update Progress Obstacle.cs

--- Items/Progress Obstacle.cs
+++ Items/Progress Obstacle.cs
@@ -1,6 +1,7 @@
-using UnityEngine;
 using Cysharp.Threading.Tasks;
 using GwambaPrimeAdventure.Connection;
+using System.Threading;
+using UnityEngine;
 namespace GwambaPrimeAdventure.Item
 {
 	[DisallowMultipleComponent, RequireComponent( typeof( Collider2D ) )]
@@ -12,6 +13,10 @@ namespace GwambaPrimeAdventure.Item
 		[SerializeField, Tooltip( "If this object will be saved as already existent object." )] private bool _saveOnSpecifics;
 		public async UniTask Load()
 		{
+			CancellationToken destroyToken = this.GetCancellationTokenOnDestroy();
+			await UniTask.Yield( PlayerLoopTiming.EarlyUpdate, destroyToken, true ).SuppressCancellationThrow();
+			if ( destroyToken.IsCancellationRequested )
+				return;
 			SaveController.Load( out SaveFile saveFile );
 			if ( _isBossProgress ? saveFile.DeafetedBosses[ _progressIndex - 1 ] : saveFile.LevelsCompleted[ _progressIndex - 1 ] )
 			{
@@ -22,7 +27,6 @@ namespace GwambaPrimeAdventure.Item
 				}
 				Destroy( gameObject );
 			}
-			await UniTask.WaitForEndOfFrame();
 		}
 	};
 };
