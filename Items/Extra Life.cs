ac7df5554098d100970ebe17e097011c4dcf8da4
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-22 21:34:56 -0300

    Update Extra Life.cs

--- Items/Extra Life.cs
+++ Items/Extra Life.cs
@@ -1,6 +1,7 @@
-using UnityEngine;
 using Cysharp.Threading.Tasks;
 using GwambaPrimeAdventure.Connection;
+using System.Threading;
+using UnityEngine;
 namespace GwambaPrimeAdventure.Item
 {
 	[DisallowMultipleComponent, RequireComponent( typeof( SpriteRenderer ), typeof( BoxCollider2D ) )]
@@ -10,10 +11,13 @@ namespace GwambaPrimeAdventure.Item
 		[SerializeField, Tooltip( "If this object will be saved as already existent object." )] private bool _saveOnSpecifics;
 		public async UniTask Load()
 		{
+			CancellationToken destroyToken = this.GetCancellationTokenOnDestroy();
+			await UniTask.Yield( PlayerLoopTiming.EarlyUpdate, destroyToken, true ).SuppressCancellationThrow();
+			if ( destroyToken.IsCancellationRequested )
+				return;
 			SaveController.Load( out SaveFile saveFile );
 			if ( saveFile.LifesAcquired.Contains( name ) )
 				Destroy( gameObject );
-			await UniTask.WaitForEndOfFrame();
 		}
 		public void Collect()
 		{
