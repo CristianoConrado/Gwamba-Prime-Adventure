c54e55c270d988efd2129c21f1a68bdff2bc7484
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-22 22:21:45 -0300

    Update Point Setter.cs

--- Characters/Point Setter.cs
+++ Characters/Point Setter.cs
@@ -16,7 +16,7 @@ namespace GwambaPrimeAdventure.Character
 		public async UniTask Load()
 		{
 			CancellationToken destroyToken = this.GetCancellationTokenOnDestroy();
-			await UniTask.Yield( PlayerLoopTiming.EarlyUpdate, destroyToken, true ).SuppressCancellationThrow();
+			await UniTask.Yield( PlayerLoopTiming.EarlyUpdate, destroyToken ).SuppressCancellationThrow();
 			if ( destroyToken.IsCancellationRequested )
 				return;
 			SaveController.Load( out SaveFile saveFile );
