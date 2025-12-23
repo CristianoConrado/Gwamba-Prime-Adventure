ad4b65c2bb12f8303659e910990cc8044e83d938
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-22 23:13:16 -0300

    Update Defender Enemy.cs

--- Enemies/Defender Enemy.cs
+++ Enemies/Defender Enemy.cs
@@ -25,7 +25,7 @@ namespace GwambaPrimeAdventure.Enemy
 		public async UniTask Load()
 		{
 			CancellationToken destroyToken = this.GetCancellationTokenOnDestroy();
-			await UniTask.Yield( PlayerLoopTiming.EarlyUpdate, destroyToken ).SuppressCancellationThrow();
+			await UniTask.Yield( PlayerLoopTiming.EarlyUpdate, destroyToken, true ).SuppressCancellationThrow();
 			if ( destroyToken.IsCancellationRequested )
 				return;
 			_timeOperation = _statistics.TimeToInvencible;
