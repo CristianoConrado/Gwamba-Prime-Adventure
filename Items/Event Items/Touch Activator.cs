4ffc2d340931f85c10522e5e9bc03c639692384e
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-26 04:59:17 -0300

    Update Touch Activator.cs

--- Items/Event Items/Touch Activator.cs
+++ Items/Event Items/Touch Activator.cs
@@ -4,13 +4,18 @@ namespace GwambaPrimeAdventure.Item.EventItem
 	[DisallowMultipleComponent, RequireComponent( typeof( Collider2D ) )]
 	internal sealed class TouchActivator : Activator
 	{
-		[Header( "Touch Activator" )]
-		[SerializeField, Tooltip( "The variety of objects that this activator can be activated." )] private GameObject[] _objectsActivators;
-		[SerializeField, Tooltip( "If this activator will be activated on a enter of a collision." )] private bool _enterCollision;
-		[SerializeField, Tooltip( "If this activator will be activated on a exit of a collision." )] private bool _exitCollision;
-		[SerializeField, Tooltip( "If this activator will be activated on a enter of a trigger collision." )] private bool _enterTrigger;
-		[SerializeField, Tooltip( "If this activator will be activated on a exit of a trigger collision." )] private bool _exitTrigger;
-		[SerializeField, Tooltip( "If this activator will destroy the object that activated the activator." )] private bool _destroyObject;
+		[SerializeField, Tooltip( "The variety of objects that this activator can be activated." ), Header( "Touch Activator" )] private GameObject[]
+			_objectsActivators;
+		[SerializeField, Tooltip( "If this activator will be activated on a enter of a collision." )] private bool
+			_enterCollision;
+		[SerializeField, Tooltip( "If this activator will be activated on a exit of a collision." )] private bool
+			_exitCollision;
+		[SerializeField, Tooltip( "If this activator will be activated on a enter of a trigger collision." )] private bool
+			_enterTrigger;
+		[SerializeField, Tooltip( "If this activator will be activated on a exit of a trigger collision." )] private bool
+			_exitTrigger;
+		[SerializeField, Tooltip( "If this activator will destroy the object that activated the activator." )] private bool
+			_destroyObject;
 		private void Activate( bool activationKey, GameObject objectKey )
 		{
 			if ( activationKey && Usable )
