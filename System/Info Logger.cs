177fc9869bb2ae8276442909d1460314b6e019e1
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-26 03:53:12 -0300

    Update Info Logger.cs

--- System/Info Logger.cs
+++ System/Info Logger.cs
@@ -6,8 +6,8 @@ namespace GwambaPrimeAdventure
 {
 	public interface IInfoLogger
 	{
-		public void LogInfo( object message, [CallerMemberName] string member = "", [CallerFilePath] string file = "", [CallerLineNumber] int line = 0 )
-			=> Debug.Log( $"[{Path.GetFileName( file )} : {line} - {member}] " + message );
+		public void LogInfo( object message, [CallerMemberName] string member = "", [CallerFilePath] string file = "", [CallerLineNumber] int line = 0 ) =>
+			Debug.Log( $"[{Path.GetFileName( file )} : {line} - {member}] " + message );
 	};
 	public sealed class InfoLogger : IInfoLogger
 	{
