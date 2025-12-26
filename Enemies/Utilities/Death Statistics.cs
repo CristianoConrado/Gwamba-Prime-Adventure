3dda141937a064971517e29b30ef8b37869b7545
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-26 05:54:31 -0300

    Update Death Statistics.cs

--- Enemies/Utilities/Death Statistics.cs
+++ Enemies/Utilities/Death Statistics.cs
@@ -4,11 +4,16 @@ namespace GwambaPrimeAdventure.Enemy.Supply
 	[CreateAssetMenu( fileName = "Death Enemy", menuName = "Enemy Statistics/Death", order = 10 )]
 	public sealed class DeathStatistics : ScriptableObject
 	{
-		[field: SerializeField, Tooltip( "The enemy that this enemy will spawn in death." ), Header( "Death Enemy", order = 0 ), Space( WorldBuild.FIELD_SPACE_LENGTH * 2F, order = 1 )]
-		public Control ChildEnemy { get; private set; }
-		[field: SerializeField, Tooltip( "The projectile that this enemy will spawn in death." )] public Projectile ChildProjectile { get; private set; }
-		[field: SerializeField, Tooltip( "The point to where spawn the object on death relative to this enemy." )] public Vector2 SpawnPoint { get; private set; }
-		[field: SerializeField, Tooltip( "If this enemy will die on touch." )] public bool OnTouch { get; private set; }
-		[field: SerializeField, Tooltip( "The time to this enemy die." )] public float TimeToDie { get; private set; }
+		[field: SerializeField, Tooltip( "The enemy that this enemy will spawn in death." ), Header( "Death Enemy", order = 0 ),
+			Space( WorldBuild.FIELD_SPACE_LENGTH * 2F, order = 1 )] public Control
+			ChildEnemy { get; private set; }
+		[field: SerializeField, Tooltip( "The projectile that this enemy will spawn in death." )] public Projectile
+			ChildProjectile { get; private set; }
+		[field: SerializeField, Tooltip( "The point to where spawn the object on death relative to this enemy." )] public Vector2
+			SpawnPoint { get; private set; }
+		[field: SerializeField, Tooltip( "If this enemy will die on touch." )] public bool
+			OnTouch { get; private set; }
+		[field: SerializeField, Tooltip( "The time to this enemy die." )] public float
+			TimeToDie { get; private set; }
 	};
 };
