e4cf915fda34bf33cb43b0610fbdae769121c0a1
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-26 03:55:01 -0300

    Update Surface.cs

--- System/Surface.cs
+++ System/Surface.cs
@@ -6,16 +6,19 @@ using UnityEngine.Events;
 using UnityEngine.Tilemaps;
 namespace GwambaPrimeAdventure
 {
-	[DisallowMultipleComponent, Icon( WorldBuild.PROJECT_ICON ), RequireComponent( typeof( Transform ), typeof( Tilemap ), typeof( TilemapRenderer ) ), RequireComponent( typeof( TilemapCollider2D ) )]
+	[DisallowMultipleComponent, Icon( WorldBuild.PROJECT_ICON ), RequireComponent( typeof( Transform ), typeof( Tilemap ), typeof( TilemapRenderer ) )]
+	[RequireComponent( typeof( TilemapCollider2D ) )]
 	public sealed class Surface : MonoBehaviour, ILoader
 	{
 		private Tilemap _tilemap;
 		private TilemapCollider2D _collider;
 		private Tile _returnedTile;
-		private readonly Dictionary<Tile, AudioClip> _tiles = new Dictionary<Tile, AudioClip>();
+		private readonly Dictionary<Tile, AudioClip>
+			_tiles = new Dictionary<Tile, AudioClip>();
 		private static AudioClip _surfaceSoundClip;
 		private static UnityAction<Vector2> _getSurface;
-		[SerializeField, Tooltip( "The sounds of the surfaces that will be played." )] private SurfaceSound[] _surfaceSounds;
+		[SerializeField, Tooltip( "The sounds of the surfaces that will be played." )] private SurfaceSound[]
+			_surfaceSounds;
 		private void OnEnable() => _getSurface += CheckPoint;
 		private void OnDisable() => _getSurface -= CheckPoint;
 		public async UniTask Load()
@@ -32,8 +35,9 @@ namespace GwambaPrimeAdventure
 		}
 		private void CheckPoint( Vector2 originPosition )
 		{
-			if ( _collider.OverlapPoint( originPosition ) && ( _returnedTile = _tilemap.GetTile<Tile>( _tilemap.WorldToCell( originPosition ) ) ) && _tiles.ContainsKey( _returnedTile ) )
-				_surfaceSoundClip = _tiles[ _returnedTile ];
+			if ( _collider.OverlapPoint( originPosition ) )
+				if ( ( _returnedTile = _tilemap.GetTile<Tile>( _tilemap.WorldToCell( originPosition ) ) ) && _tiles.ContainsKey( _returnedTile ) )
+					_surfaceSoundClip = _tiles[ _returnedTile ];
 		}
 		public static AudioClip CheckSurface( Vector2 originPosition )
 		{
