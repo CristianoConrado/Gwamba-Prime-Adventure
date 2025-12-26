5bca68abcd9551349da83998a9d1826390c269f0
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-26 04:57:36 -0300

    Update Destruction Activator.cs

--- Items/Event Items/Destruction Activator.cs
+++ Items/Event Items/Destruction Activator.cs
@@ -4,7 +4,8 @@ namespace GwambaPrimeAdventure.Item.EventItem
 	[DisallowMultipleComponent]
 	internal sealed class DestructionActivator : Activator
 	{
-		[SerializeField, Tooltip( "If this activator will activate after the destruction." ), Header( "Destruction Activator" )] private bool _activate;
+		[SerializeField, Tooltip( "If this activator will activate after the destruction." ), Header( "Destruction Activator" )] private bool
+			_activate;
 		private new void OnDestroy()
 		{
 			base.OnDestroy();
