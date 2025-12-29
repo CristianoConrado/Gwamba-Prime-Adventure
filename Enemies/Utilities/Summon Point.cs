797f5391c891a483f2349075ea132fa378510e3d
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-29 19:28:53 -0300

    Update Summon Point.cs

--- Enemies/Utilities/Summon Point.cs
+++ Enemies/Utilities/Summon Point.cs
@@ -5,11 +5,15 @@ namespace GwambaPrimeAdventure.Enemy.Supply
 	[DisallowMultipleComponent, RequireComponent( typeof( Collider2D ) )]
 	public sealed class SummonPoint : StateController
 	{
-		private ISummoner _summoner;
-		private ushort _summonIndex;
-		[SerializeField, Tooltip( "If this point will destroy itself after use." ), Header( "Interactions" )] private bool
+		private
+			ISummoner _summoner;
+		private
+			ushort _summonIndex;
+		[SerializeField, Tooltip( "If this point will destroy itself after use." ), Header( "Interactions" )]
+		private bool
 			_destroyAfter;
-		[SerializeField, Tooltip( "If this point will trigger with other object." )] private bool
+		[SerializeField, Tooltip( "If this point will trigger with other object." )]
+		private bool
 			_hasTarget;
 		public void GetTouch( ISummoner summoner, ushort summonIndex )
 		{
