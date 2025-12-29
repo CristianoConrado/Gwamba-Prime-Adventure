e3ef8dad2b7924270ad6a23e37e83adf61443fe9
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-29 16:05:20 -0300

    Update Extra Life.cs

--- Items/Extra Life.cs
+++ Items/Extra Life.cs
@@ -7,7 +7,8 @@ namespace GwambaPrimeAdventure.Item
 	[DisallowMultipleComponent, RequireComponent( typeof( SpriteRenderer ), typeof( BoxCollider2D ) )]
 	internal sealed class ExtraLife : StateController, ILoader, ICollectable
 	{
-		[SerializeField, Tooltip( "If this object will be saved as already existent object." ), Header( "Condition" )] private bool
+		[SerializeField, Tooltip( "If this object will be saved as already existent object." ), Header( "Condition" )]
+		private bool
 			_saveOnSpecifics;
 		public async UniTask Load()
 		{
