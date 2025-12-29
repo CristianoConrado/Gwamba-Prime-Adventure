1e4a0964c83b3e235431c0273c16f0877231a892
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-29 14:31:12 -0300

    Update State Controller.cs

--- System/State Controller.cs
+++ System/State Controller.cs
@@ -5,7 +5,8 @@ namespace GwambaPrimeAdventure
 	[Icon( WorldBuild.PROJECT_ICON ), RequireComponent( typeof( Transform ) )]
 	public abstract class StateController : MonoBehaviour
 	{
-		private static UnityAction<bool> _setState;
+		private static
+			UnityAction<bool> _setState;
 		protected void Awake() => _setState += NewState;
 		protected void OnDestroy() => _setState -= NewState;
 		private void NewState( bool state ) => enabled = state;
