87e64cf55c3095b1dc69218c1fd1d184a373b270
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-29 14:06:43 -0300

    Update Gwamba Damager.cs

--- Characters/Gwamba Damager.cs
+++ Characters/Gwamba Damager.cs
@@ -10,25 +10,46 @@ namespace GwambaPrimeAdventure.Character
 	[DisallowMultipleComponent, RequireComponent( typeof( SpriteRenderer ), typeof( Collider2D ) )]
 	internal sealed class GwambaDamager : StateController, IDestructible
 	{
-		private SpriteRenderer _spriteRenderer;
+		private
+			SpriteRenderer _spriteRenderer;
 		internal readonly HashSet<IDestructible>
 			damagedes = new HashSet<IDestructible>();
-		internal event Predicate<ushort> DamagerHurt;
-		internal event UnityAction<ushort, float> DamagerStun;
-		internal event UnityAction<GwambaDamager, IDestructible> DamagerAttack;
+		internal event
+			Predicate<ushort> DamagerHurt;
+		internal event
+			UnityAction<ushort, float> DamagerStun;
+		internal event
+			UnityAction<GwambaDamager, IDestructible> DamagerAttack;
 		private Color
 			_alphaChanger = new Color();
-		[SerializeField, Tooltip( "If this Gwamba's part will take damage." ), Space( WorldBuild.FIELD_SPACE_LENGTH * 2F )] private bool
-			_takeDamage;
-		[field: SerializeField, HideIf( nameof( _takeDamage ) ), Tooltip( "The velocity of the screen shake on the attack." )] internal Vector2
-			AttackShake { get; private set; }
-		[field: SerializeField, HideIf( nameof( _takeDamage ) ), Tooltip( "The amount of damage that the attack of Gwamba hits." )] internal ushort
-			AttackDamage { get; private set; }
-		[field: SerializeField, HideIf( nameof( _takeDamage ) ), Min( 0F ), Tooltip( "The amount of time the attack screen shake will be applied." )] internal float
-			AttackShakeTime { get; private set; }
-		[field: SerializeField, HideIf( nameof( _takeDamage ) ), Min( 0F ), Tooltip( "The amount of time that this Gwamba's attack stun does." )] internal float
-			StunTime { get; private set; }
-		internal float Alpha
+		[SerializeField, Tooltip( "If this Gwamba's part will take damage." ), Space( WorldBuild.FIELD_SPACE_LENGTH * 2F )]
+		private
+			bool _takeDamage;
+		[field: SerializeField, HideIf( nameof( _takeDamage ) ), Tooltip( "The velocity of the screen shake on the attack." )]
+		internal Vector2 AttackShake
+        {
+            get;
+            private set;
+        }
+        [field: SerializeField, HideIf( nameof( _takeDamage ) ), Tooltip( "The amount of damage that the attack of Gwamba hits." )]
+		internal ushort AttackDamage
+        {
+            get;
+            private set;
+        }
+        [field: SerializeField, HideIf( nameof( _takeDamage ) ), Min( 0F ), Tooltip( "The amount of time the attack screen shake will be applied." )]
+		internal float AttackShakeTime
+        {
+            get;
+            private set;
+        }
+        [field: SerializeField, HideIf( nameof( _takeDamage ) ), Min( 0F ), Tooltip( "The amount of time that this Gwamba's attack stun does." )]
+		internal float StunTime
+        {
+            get;
+            private set;
+        }
+        internal float Alpha
 		{
 			get => _spriteRenderer ? _spriteRenderer.color.a : 0F;
 			set
