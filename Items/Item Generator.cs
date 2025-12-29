01fe58bbd5e51aa021a84984dbba2fd1c974a63a
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-29 16:05:37 -0300

    Update Item Generator.cs

--- Items/Item Generator.cs
+++ Items/Item Generator.cs
@@ -12,17 +12,23 @@ namespace GwambaPrimeAdventure.Enemy
 			_timeGeneration = 0F;
 		private bool
 			_continueGeneration = true;
-		[SerializeField, Tooltip( "The item to be generated." ), Header( "Generation Statistics" )] private GameObject
+		[SerializeField, Tooltip( "The item to be generated." ), Header( "Generation Statistics" )]
+		private GameObject
 			_generatedItem;
-		[SerializeField, Tooltip( "The amount of items that have to be generated." )] private ushort
+		[SerializeField, Tooltip( "The amount of items that have to be generated." )]
+		private ushort
 			_quantityToGenerate;
-		[SerializeField, Tooltip( "The amount of time to waits to generation." )] private float
+		[SerializeField, Tooltip( "The amount of time to waits to generation." )]
+		private float
 			_generationTime;
-		[SerializeField, Tooltip( "If the items generated are to be keeped in existence." )] private bool
+		[SerializeField, Tooltip( "If the items generated are to be keeped in existence." )]
+		private bool
 			_existentItems;
-		[SerializeField, HideIf( nameof( _existentItems ) ), Tooltip( "If the quantity of the generation is limited." )] private bool
+		[SerializeField, HideIf( nameof( _existentItems ) ), Tooltip( "If the quantity of the generation is limited." )]
+		private bool
 			_especifiedGeneration;
-		[SerializeField, HideIf( nameof( _existentItems ) ), ShowIf( nameof( _especifiedGeneration ) ), Tooltip( "If this generator will destroy the entire object." )] private bool
+		[SerializeField, HideIf( nameof( _existentItems ) ), ShowIf( nameof( _especifiedGeneration ) ), Tooltip( "If this generator will destroy the entire object." )]
+		private bool
 			_destroyObject;
 		private void Update()
 		{
