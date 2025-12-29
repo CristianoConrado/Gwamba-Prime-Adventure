49d55bf9c955e90de7be9200e32cc4bf82cc6f34
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-29 14:07:42 -0300

    Update Point Setter.cs

--- Characters/Point Setter.cs
+++ Characters/Point Setter.cs
@@ -7,13 +7,17 @@ namespace GwambaPrimeAdventure.Character
 	[DisallowMultipleComponent, RequireComponent( typeof( BoxCollider2D ) )]
 	internal sealed class PointSetter : StateController, ILoader
 	{
-		private static PointSetter Instance;
-		[SerializeField, Tooltip( "The name of the hubby world scene." ), Space( WorldBuild.FIELD_SPACE_LENGTH * 2F )] private SceneField 
-			_hubbyWorldScene;
-		[SerializeField, Tooltip( "If this point is faced to left." )] private bool
-			_turnToLeft;
-		[SerializeField, Tooltip( "Which point setter is setted when scene is the hubby world." )] private ushort
-			_selfIndex;
+		private static
+			PointSetter Instance;
+		[SerializeField, Tooltip( "The name of the hubby world scene." ), Space( WorldBuild.FIELD_SPACE_LENGTH * 2F )]
+		private
+			SceneField _hubbyWorldScene;
+		[SerializeField, Tooltip( "If this point is faced to left." )]
+		private
+			bool _turnToLeft;
+		[SerializeField, Tooltip( "Which point setter is setted when scene is the hubby world." )]
+		private
+			ushort _selfIndex;
 		internal static Vector2 CheckedPoint =>
 			Instance ? Instance.transform.position : Vector2.zero;
 		internal static bool TurnToLeft =>
@@ -25,9 +29,8 @@ namespace GwambaPrimeAdventure.Character
 			if ( destroyToken.IsCancellationRequested )
 				return;
 			SaveController.Load( out SaveFile saveFile );
-			if ( gameObject.scene.name == _hubbyWorldScene && !string.IsNullOrEmpty( saveFile.LastLevelEntered ) )
-				if ( saveFile.LastLevelEntered.Contains( $"{_selfIndex}" ) )
-					Instance = this;
+			if ( gameObject.scene.name == _hubbyWorldScene && !string.IsNullOrEmpty( saveFile.LastLevelEntered ) && saveFile.LastLevelEntered.Contains( $"{_selfIndex}" ) )
+				Instance = this;
 		}
 		private void OnTriggerEnter2D( Collider2D other )
 		{
