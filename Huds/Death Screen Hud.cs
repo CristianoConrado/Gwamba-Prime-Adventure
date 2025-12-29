dda612f8ab654250b538a9f1a5b77692462f1e6e
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-29 15:56:41 -0300

    Update Death Screen Hud.cs

--- Huds/Death Screen Hud.cs
+++ Huds/Death Screen Hud.cs
@@ -5,14 +5,39 @@ namespace GwambaPrimeAdventure.Hud
 	[DisallowMultipleComponent, Icon( WorldBuild.PROJECT_ICON ), RequireComponent( typeof( Transform ), typeof( UIDocument ) )]
 	internal sealed class DeathScreenHud : MonoBehaviour
 	{
-		private static DeathScreenHud _instance;
-		internal VisualElement RootElement { get; private set; }
-		internal VisualElement Curtain { get; private set; }
-		internal Label Text { get; private set; }
-		internal Button Continue { get; private set; }
-		internal Button OutLevel { get; private set; }
-		internal Button GameOver { get; private set; }
-		private void Awake()
+		private static
+			DeathScreenHud _instance;
+		internal VisualElement RootElement
+        {
+            get;
+            private set;
+        }
+        internal VisualElement Curtain
+        {
+            get;
+            private set;
+        }
+        internal Label Text
+        {
+            get;
+            private set;
+        }
+        internal Button Continue
+        {
+            get;
+            private set;
+        }
+        internal Button OutLevel
+        {
+            get;
+            private set;
+        }
+        internal Button GameOver
+        {
+            get;
+            private set;
+        }
+        private void Awake()
 		{
 			if ( _instance )
 			{
