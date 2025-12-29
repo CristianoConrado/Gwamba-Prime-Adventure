53acc20a0175c3525baf2735a062c888b492a623
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-29 16:13:13 -0300

    Update Story Scene Object.cs

--- Story/Story Scene Object.cs
+++ Story/Story Scene Object.cs
@@ -6,18 +6,38 @@ namespace GwambaPrimeAdventure.Story
 	internal sealed class StorySceneObject : ScriptableObject
 	{
 		[field: SerializeField, Tooltip( "The collection of objects that display the scenes." ), Header( "Scene Collection" ), Space( WorldBuild.FIELD_SPACE_LENGTH * 2F )]
-		internal SceneComponent[] SceneComponents { get; private set; }
+		internal SceneComponent[] SceneComponents
+		{
+			get;
+			private set;
+		}
 		[Serializable]
 		internal struct SceneComponent
 		{
-			[field: SerializeField, Tooltip( "The main imgae that is placed in the hud." ), Header( "Components" ), Space( WorldBuild.FIELD_SPACE_LENGTH * 2F )] internal Texture2D
-				Image { get; private set; }
-			[field: SerializeField, Tooltip( "If the dialog will go turned off during the scene." )] internal bool
-				OffDialog { get; private set; }
-			[field: SerializeField, Tooltip( "The amount of time that the dialog will be turned off." )] internal float
-				TimeToDesapear { get; private set; }
-			[field: SerializeField, Tooltip( "If the dialog will jump to the next slide." )] internal bool
-				JumpToNext { get; private set; }
-		};
+			[field: SerializeField, Tooltip( "The main imgae that is placed in the hud." ), Header( "Components" ), Space( WorldBuild.FIELD_SPACE_LENGTH * 2F )]
+			internal Texture2D Image
+            {
+                get;
+                private set;
+            }
+            [field: SerializeField, Tooltip( "If the dialog will go turned off during the scene." )]
+			internal bool OffDialog
+            {
+                get;
+                private set;
+            }
+            [field: SerializeField, Tooltip( "The amount of time that the dialog will be turned off." )]
+			internal float TimeToDesapear
+            {
+                get;
+                private set;
+            }
+            [field: SerializeField, Tooltip( "If the dialog will jump to the next slide." )]
+			internal bool JumpToNext
+            {
+                get;
+                private set;
+            }
+        };
 	};
 };
