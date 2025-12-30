45968ac7cea8f07dcbfb1547c747dcf091ef6431
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2025-12-29 22:29:39 -0300

    Update Item Generator.cs

--- Items/Item Generator.cs
+++ Items/Item Generator.cs
@@ -13,23 +13,23 @@ namespace GwambaPrimeAdventure.Enemy
 		private bool
 			_continueGeneration = true;
 		[SerializeField, Tooltip( "The item to be generated." ), Header( "Generation Statistics" )]
-		private GameObject
-			_generatedItem;
+		private
+			GameObject _generatedItem;
 		[SerializeField, Tooltip( "The amount of items that have to be generated." )]
-		private ushort
-			_quantityToGenerate;
+		private
+			ushort _quantityToGenerate;
 		[SerializeField, Tooltip( "The amount of time to waits to generation." )]
-		private float
-			_generationTime;
+		private
+			float _generationTime;
 		[SerializeField, Tooltip( "If the items generated are to be keeped in existence." )]
-		private bool
-			_existentItems;
+		private
+			bool _existentItems;
 		[SerializeField, HideIf( nameof( _existentItems ) ), Tooltip( "If the quantity of the generation is limited." )]
-		private bool
-			_especifiedGeneration;
+		private
+			bool _especifiedGeneration;
 		[SerializeField, HideIf( nameof( _existentItems ) ), ShowIf( nameof( _especifiedGeneration ) ), Tooltip( "If this generator will destroy the entire object." )]
-		private bool
-			_destroyObject;
+		private
+			bool _destroyObject;
 		private void Update()
 		{
 			if ( _continueGeneration && 0F < _timeGeneration )
