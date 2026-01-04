51b301ad98ff188b40f9148c458850ba165f1799
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2026-01-04 13:30:20 -0300

    Update Outer Enemy.cs

--- Enemies/Outer Enemy.cs
+++ Enemies/Outer Enemy.cs
@@ -1,26 +1,29 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.Events;
 namespace GwambaPrimeAdventure.Enemy
 {
 	[DisallowMultipleComponent, RequireComponent( typeof( Collider2D ) )]
 	internal sealed class OuterEnemy : StateController, IDestructible
 	{
-		private IDestructible _destructibleEnemy;
 		private UnityAction<Collider2D> _hitEvent;
 		[SerializeField, Tooltip( "Is this enemy can hit other objects." )]
 		private
 			bool _isOnlyHit;
-		public IDestructible Source => _destructibleEnemy;
-		public short Health => (short) _destructibleEnemy?.Health;
+		public IDestructible Source
+		{
+			get;
+			private set;
+		}
+		public short Health => (short) Source?.Health;
 		internal void Deliver( IDestructible destructibleEnemy, UnityAction<Collider2D> hitEvent )
 		{
 			_hitEvent = hitEvent;
 			if ( _isOnlyHit )
 				return;
-			_destructibleEnemy = destructibleEnemy;
+			Source = destructibleEnemy;
 		}
-		public bool Hurt( ushort damage ) => (bool) _destructibleEnemy?.Hurt( damage );
-		public void Stun( ushort stunStrenght, float stunTime ) => _destructibleEnemy?.Stun( stunStrenght, stunTime );
+		public bool Hurt( ushort damage ) => (bool) Source?.Hurt( damage );
+		public void Stun( ushort stunStrenght, float stunTime ) => Source?.Stun( stunStrenght, stunTime );
 		private void OnTriggerEnter2D( Collider2D other ) => _hitEvent?.Invoke( other );
 	};
 };
