97085eafcca4d23777f26212089c795c6761884e
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2026-01-31 15:52:12 -0300

    Refactor MenuHud class for improved readability

--- Huds/Menu Hud.cs
+++ Huds/Menu Hud.cs
@@ -6,58 +6,58 @@ namespace GwambaPrimeAdventure.Hud
 	internal sealed class MenuHud : MonoBehaviour
 	{
 		private static
-            MenuHud _instance;
+		  MenuHud _instance;
 		internal GroupBox Buttons
 		{
 			get;
 			private set;
 		}
 		internal GroupBox Saves
-        {
-            get;
-            private set;
-        }
-        internal Button Play
-        {
-            get;
-            private set;
-        }
-        internal Button Configurations
-        {
-            get;
-            private set;
-        }
-        internal Button Quit
-        {
-            get;
-            private set;
-        }
-        internal Button Back
-        {
-            get;
-            private set;
-        }
-        internal TextField[] SaveName
-        {
-            get;
-            private set;
-        }
-        internal Button[] RenameFile
-        {
-            get;
-            private set;
-        }
-        internal Button[] Load
-        {
-            get;
-            private set;
-        }
-        internal Button[] Delete
-        {
-            get;
-            private set;
-        }
-        private void Awake()
+		{
+			get;
+			private set;
+		}
+		internal Button Play
+		{
+			get;
+			private set;
+		}
+		internal Button Configurations
+		{
+			get;
+			private set;
+		}
+		internal Button Quit
+		{
+			get;
+			private set;
+		}
+		internal Button Back
+		{
+			get;
+			private set;
+		}
+		internal TextField[] SaveName
+		{
+			get;
+			private set;
+		}
+		internal Button[] RenameFile
+		{
+			get;
+			private set;
+		}
+		internal Button[] Load
+		{
+			get;
+			private set;
+		}
+		internal Button[] Delete
+		{
+			get;
+			private set;
+		}
+		private void Awake()
 		{
 			if ( _instance )
 			{
@@ -73,17 +73,17 @@ namespace GwambaPrimeAdventure.Hud
 			Quit = root.Q<Button>( nameof( Quit ) );
 			Back = root.Q<Button>( nameof( Back ) );
 			SaveName = new TextField[ 4 ];
-			for ( ushort i = 0; i < SaveName.Length; i++ )
-				SaveName[ i ] = root.Q<TextField>( $"{nameof( SaveName )}{i + 1}" );
+			for ( ushort i = 0; SaveName.Length > i++; )
+				SaveName[ i ] = root.Q<TextField>( $"{nameof( SaveName )}{1 + i}" );
 			RenameFile = new Button[ 4 ];
-			for ( ushort i = 0; i < RenameFile.Length; i++ )
-				RenameFile[ i ] = root.Q<Button>( $"{nameof( RenameFile )}{i + 1}" );
+			for ( ushort i = 0; SaveName.Length > i++; )
+				RenameFile[ i ] = root.Q<Button>( $"{nameof( RenameFile )}{1 + i}" );
 			Load = new Button[ 4 ];
-			for ( ushort i = 0; i < Load.Length; i++ )
-				Load[ i ] = root.Q<Button>( $"{nameof( Load )}{i + 1}" );
+			for ( ushort i = 0; Load.Length > i++; )
+				Load[ i ] = root.Q<Button>( $"{nameof( Load )}{1 + i}" );
 			Delete = new Button[ 4 ];
-			for ( ushort i = 0; i < Delete.Length; i++ )
-				Delete[ i ] = root.Q<Button>( $"{nameof( Delete )}{i + 1}" );
+			for ( ushort i = 0; SaveName.Length > i++; )
+				Delete[ i ] = root.Q<Button>( $"{nameof( Delete )}{1 + i}" );
 		}
 	};
 };
