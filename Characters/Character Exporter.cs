eb8cba2b8240262d6046420adfb880a4ca9ebc6d
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2026-02-13 18:06:27 -0300

    Update Character Exporter.cs

--- Characters/Character Exporter.cs
+++ Characters/Character Exporter.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 namespace GwambaPrimeAdventure.Character
 {
-    public static class CharacterExporter
-    {
-		public static Vector2 GwambaLocalization() => GwambaState<GwambaMarker>.Instance ? GwambaState<GwambaMarker>.Instance.transform.position : Vector2.zero;
-		public static bool EqualGwamba( params GameObject[] others ) => GwambaState<GwambaMarker>.Instance ? GwambaState<GwambaMarker>.Instance.EqualObject( others ) : false;
+	public static class CharacterExporter
+	{
+		public static Vector2 GwambaLocalization() => GwambaMarker.Instance ? GwambaMarker.Instance.transform.position : Vector2.zero;
+		public static bool EqualGwamba( params GameObject[] others ) => GwambaMarker.Instance ? GwambaMarker.Instance.EqualObject( others ) : false;
 	};
 };
