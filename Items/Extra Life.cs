68f2798209cf2002a0419a6b3a5b84c09888f0f9
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2026-02-15 16:57:16 -0300

    Update Extra Life.cs

--- Items/Extra Life.cs
+++ Items/Extra Life.cs
@@ -1,21 +1,15 @@
-using Cysharp.Threading.Tasks;
 using GwambaPrimeAdventure.Connection;
-using System.Threading;
 using UnityEngine;
 namespace GwambaPrimeAdventure.Item
 {
 	[DisallowMultipleComponent, RequireComponent( typeof( SpriteRenderer ), typeof( BoxCollider2D ) )]
-	internal sealed class ExtraLife : StateController, ILoader, ICollectable
+	internal sealed class ExtraLife : StateController, ICollectable
 	{
 		[SerializeField, Tooltip( "If this object will be saved as already existent object." ), Header( "Condition" )]
 		private
 			bool _saveOnSpecifics;
-		public async UniTask Load()
+		private void Start()
 		{
-			CancellationToken destroyToken = this.GetCancellationTokenOnDestroy();
-			await UniTask.Yield( PlayerLoopTiming.EarlyUpdate, destroyToken, true ).SuppressCancellationThrow();
-			if ( destroyToken.IsCancellationRequested )
-				return;
 			SaveController.Load( out SaveFile saveFile );
 			if ( saveFile.LifesAcquired.Contains( name ) )
 				Destroy( gameObject );
