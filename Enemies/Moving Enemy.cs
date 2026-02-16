e255cad4be901268a45d9d2c973641c5bea1445e
Cristiano Conrado <129303913+CristianoConrado@users.noreply.github.com>
2026-02-16 00:28:37 -0300

    Update Moving Enemy.cs

--- Enemies/Moving Enemy.cs
+++ Enemies/Moving Enemy.cs
@@ -7,7 +7,7 @@ namespace GwambaPrimeAdventure.Enemy
 	internal abstract class MovingEnemy : EnemyProvider, IConnector
 	{
 		protected readonly List<ContactPoint2D>
-			_groundContacts = new List<ContactPoint2D>( (int) WorldBuild.PIXELS_PER_UNIT );
+			_groundContacts = new List<ContactPoint2D>( (byte) WorldBuild.PIXELS_PER_UNIT );
 		protected Vector2
 			_originCast = Vector2.zero,
 			_sizeCast = Vector2.zero;
